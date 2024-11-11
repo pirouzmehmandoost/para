36e6f218205ad3df850587cc377a08db2b01fb1c
Pirouz Mehmandoost <pirouzm@systemss-mbp.mynetworksettings.com>
2024-11-10 23:32:04 -0800

    clean up files

--- src/app/shop/page.js
+++ src/app/shop/page.js
@@ -3,25 +3,24 @@
 import ProductCard from "../components/ProductCard"
 import portfolio from "../../lib/globals"
 
-const { products } = portfolio;
 
-export default function Shop() {
+const Shop = () => {
+    const { products } = portfolio;
+
     return (
-        <div
-            className="flex flex-col w-full h-full place-items-center items-center justify-around text-center text-clay_dark bg-transparent"
-        >
-            <p className="text-5xl mb-10">
+        <div className="flex flex-col w-full h-full place-items-center items-center justify-around text-center text-clay_dark bg-transparent" >
+            <h2 className="text-5xl mb-10">
                 This is the Shop
-            </p>
-
+            </h2>
             <div className="grid grid-cols-2 gap 2">
                 {
                     products.map((item, index) => {
-                        return <ProductCard
-                            key={index} data={item} />
+                        return <ProductCard key={index} data={item} />
                     })
                 }
             </div>
         </div>
     );
-}
+};
+
+export default Shop;
--- src/app/shop/product/page.js
+++ src/app/shop/product/page.js
@@ -1,7 +1,6 @@
 "use client";
 
 import { useState } from 'react';
-
 import Image from 'next/image'
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
@@ -9,26 +8,16 @@ import ThreeDRotationIcon from '@mui/icons-material/ThreeDRotation';
 import useSelection from '../../store/selection';
 import ShopFooter from "./../../components/ShopFooter"
 
-export default function ProductViewer() {
-
+const ProductViewer = () => {
     const selection = useSelection(state => state.selection)
-
     const [increment, setIncrement] = useState(0)
-
-    const {
-        imgUrl,
-        imgUrl: [mainImage, ...rest],
-        name,
-        price,
-        modelUrl,
-    } = selection
+    const { imgUrl, name } = selection;
 
     return (
         <div
             id="product_viewer"
             className="w-full h-full flex flex-col relative  bg-transparent justify-between items-center text-center"
         >
-
             <div>
                 <Image
                     className="bg-cover overflow-auto"
@@ -39,32 +28,16 @@ export default function ProductViewer() {
                     alt={name}
                     quality={100}
 
-                // onClick={ () => { increment == imgUrl.length? setIncrement(0) : setIncrement(increment+1) }}
                 />
             </div>
-
             <div className="flex flex-row justify-between items-center text-center" >
-                <KeyboardArrowLeftIcon
-                    onClick={() => { increment <= 0 ? setIncrement(imgUrl.length - 1) : setIncrement(increment - 1) }}
-                />
-
+                <KeyboardArrowLeftIcon onClick={() => { increment <= 0 ? setIncrement(imgUrl.length - 1) : setIncrement(increment - 1) }} />
                 <ThreeDRotationIcon />
-
-                <KeyboardArrowRightIcon
-                    onClick={() => { increment < imgUrl.length - 1 ? setIncrement(increment + 1) : setIncrement(0) }}
-                />
-            </div>
-
-            <div className=" flex flex-column justify-around items-center text-center" >
-                <ul>
-                    <li><p> Name: {name} </p></li>
-                    <li><p> Price: {price} </p></li>
-                    <li><p> Model Url: {modelUrl} </p></li>
-                    <li><p> Description: This is where a product description will go. </p></li>
-                </ul>
+                <KeyboardArrowRightIcon onClick={() => { increment < imgUrl.length - 1 ? setIncrement(increment + 1) : setIncrement(0) }} />
             </div>
             <ShopFooter />
-
         </div>
-    )
-}
+    );
+};
+
+export default ProductViewer;
