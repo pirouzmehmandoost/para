3a736ac7ce850c836aa3844962d352add2ec33dc
Pirouz Mehmandoost <pirouz.sf@gmail.com>
2024-11-17 16:55:35 -0800

    reformat ShopMenu and remove icons

--- src/app/components/ProductCard.js
+++ src/app/components/ProductCard.js
@@ -7,7 +7,6 @@ import { useState } from "react";
 
 
 const ProductCard = (props) => {
-
     const {
         data,
         data: {
--- src/app/components/ShopMenu.js
+++ src/app/components/ShopMenu.js
@@ -1,12 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import Image from 'next/image'
-import MenuIcon from "@mui/icons-material/Menu";
-import ClearIcon from "@mui/icons-material/Clear";
-
 import useSelection from "../store/selection";
-
 
 const ShopMenu = () => {
 
@@ -15,11 +10,8 @@ const ShopMenu = () => {
     const toggleExpanded = () => setExpanded(current => !current);
 
     const {
-        imgUrl,
-        imgUrl: [mainImage, ...rest],
         name,
         price,
-        modelUrl,
         colors,
         description,
     } = selection;
@@ -30,20 +22,7 @@ const ShopMenu = () => {
         if (cssColor.includes("white")) return "bg-slate-100";
         if (cssColor.includes("black")) return "bg-slate-950";
         else return "bg-lime-200"
-    }
-
-
-    const descriptionSection = (
-        <div
-            className={`flex flex-col flex-grow mb-3 text-xl text-clay_dark `}
-        >
-            <div className=" flex flex-col mb-3">
-                <ul>
-                    <li><p> {description} </p></li>
-                </ul>
-            </div>
-        </div>
-    )
+    };
 
 
     const colorSelection = (
@@ -60,42 +39,38 @@ const ShopMenu = () => {
                 })
             }
         </div>
-    )
+    );
+
 
     const wrapper = (
-        <div className={`${expanded ? "backdrop-blur-3xl  backdrop-brightness-150" : ""}`} >
-            <div className={`flex flex-row justify-between items-center text-center text-clay_dark "cursor-pointer`}>
+        <div className={` flex flex-col justify-between items-end text-clay_dark ${expanded ? "backdrop-blur-3xl  backdrop-brightness-150" : ""}`} >
+            <div className={`px-6 pt-0 overflow-hidden transition duration-100 ease-in-out ${expanded ? "max-h-full" : "max-h-0"}`} >
+                {description}
+            </div>
+
+            <div className=" flex flex-row my-3 max-w-full place-content-evenly" >
                 <div
-                >
-                    <div className={`flex flex-col px-6 pt-0 overflow-hidden transition duration-0 ${expanded ? "max-h-full" : "max-h-0"}`} >
-                        {descriptionSection}
-                    </div>
-
-                    {
-                        expanded ?
-                            <ClearIcon className=" mx-5 text-clay_dark" onClick={toggleExpanded}
-                            />
-                            :
-                            <MenuIcon className="mx-5 text-clay_dark" onClick={toggleExpanded}
-                            />
-                    }
+                    className="cursor-pointer"
+                    onClick={toggleExpanded}>
+                    <p>{name}</p>
                 </div>
-
-                <p> {name} </p>
-                <p> {price} </p>
-                {colorSelection}
-
-
+                <div>
+                    <p>{price}</p>
+                </div>
+                <div className="cursor-pointer">
+                    {colorSelection}
+                </div>
             </div>
         </div>
     );
 
+
     return (
         <div
             id="shop_menu"
-            className="w-full ml-12 mt-6 justify-between content-center items-center text-center"
+            className=" max-w-full ml-12 mb-10 justify-between content-center items-center text-center"
         >
-            <div className=" fixed z-20  bottom-0 right-0 border-solid border-2 border-clay_dark" >
+            <div className=" w-1/2 fixed z-20  bottom-0 right-0 border-solid border-2 border-clay_dark" >
                 {wrapper}
             </div>
         </div>
