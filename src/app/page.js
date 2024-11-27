72ab48a881d032160ba269811aeaf5d1d9610a28
Pirouz Mehmandoost <pirouz.sf@gmail.com>
2024-11-27 13:42:23 -0800

    remove unused props passed to ModelViewer and unused tailwind global configs

--- src/app/page.js
+++ src/app/page.js
@@ -4,13 +4,10 @@ import ModelViewer from "./components/ModelViewer";
 import useSelection from "./store/selection";
 
 export default function Home() {
-    const selection = useSelection((state) => state.selection);
-    const { modelUrl } = selection;
-
     return (
         <main className={`flex flex-col place-items-center items-center w-screen max-w-screen h-screen max-h-screen" bg-cover bg-[url(/oval_bag_hero_2.png)]`}>
             <div className=" w-4/5 h-4/5" >
-                <ModelViewer modelUrl={modelUrl} />
+                <ModelViewer />
             </div>
         </main>
     );
