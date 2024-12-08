0715c26607c40d5a92a2dd17783e9ac149e99c16
Pirouz Mehmandoost <pirouzm@systemss-mbp.mynetworksettings.com>
2024-12-08 13:25:19 -0800

    Add responsive styling for small and medium screen sizes

--- src/app/components/Menu.js
+++ src/app/components/Menu.js
@@ -3,7 +3,6 @@
 import useSelection from '../store/selection';
 
 const Menu = ({setExpanded, expanded} ) => {
-
     const selection = useSelection(state => state.selection);
     const {
         colors,
@@ -40,27 +39,26 @@ const Menu = ({setExpanded, expanded} ) => {
     return (
          < div 
             id="shop_menu"
-            className={`sticky bottom-0 z-20 right-0 w-1/2 place-self-end transition-all duration-700 ease-in-out ${expanded ? "mt-96" : "mt-0"}`}
+            className={`sticky bottom-0 z-20 right-0 sm:w-full md:w-full lg:w-1/2 place-self-end transition-all duration-700 ease-in-out ${expanded ? "mt-96" : "mt-0"}`}
         >
             <div className="flex z-20 w-full h-full bottom-0 right-0" >
                 <div className={`w-full h-full border-solid border-2 border-clay_dark`} >
                     <div className={`flex flex-col text-clay_dark backdrop-blur-xl backdrop-brightness-150 transition-all duration-500 ease-in-out ${expanded ? "backdrop-opacity-100" : "backdrop-opacity-0"}`} >
                         <div className={`px-6 pt-0 justify-items-center transition-all duration-700 ease-in-out ${expanded ? "overflow-auto max-h-96" : "overflow-hidden max-h-0"}`} >
-                            <p  className={`transition-all duration-700 ease-in-out delay-75 ${expanded ? "opacity-100" : "opacity-0"}`} >
+                            <p className={` mt-5 transition-all duration-700 ease-in-out delay-75 ${expanded ? "opacity-100" : "opacity-0"}`} >
                                 {description}
                             </p>
                         </div>
                         <div className="flex flex-row my-3 max-w-full" >
-                            <div
-                                className="justify-items-center basis-1/4 cursor-pointer" >
+                            <div className="ml-5 justify-items-center basis-1/4 text-nowrap cursor-pointer" >
                                 <p onClick={setExpanded} >
                                     {name}
                                 </p>
                             </div>
-                            <div className=" justify-items-center basis-1/4" >
+                            <div className="sm:ml-5s md:ml-5 lg:ml-5 justify-items-center basis-1/4" >
                                 <p>{price}</p>
                             </div>
-                            <div className="justify-items-center basis-1/2" >
+                            <div className="sm:mx-2 md:mx-2 justify-items-center basis-1/2" >
                                 {colorSelectButtons}
                             </div>
                         </div>
