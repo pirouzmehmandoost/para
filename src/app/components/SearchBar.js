de859b54f94b9637bef0284d7f082840bbf1ad4e
Pirouz Mehmandoost <pirouz.sf@gmail.com>
2025-01-05 00:39:18 -0800

    remove unused vars and imports in SearchBar.js

--- src/app/components/SearchBar.js
+++ src/app/components/SearchBar.js
@@ -1,17 +1,17 @@
 "use client";
 
-import { useState } from "react";
+// import { useState } from "react";
 
 const SearchBar = () => {
-    const [searchQuery, setsearchQuery] = useState("invisible");
+    // const [searchQuery, setsearchQuery] = useState("invisible");
 
     return (
         <div className="flex flex-row flex-grow grid text-center text-2xl text-clay_dark" >
-            <input 
-                type="text" 
-                id="search_input" 
-                className= "text-center bg-transparent border-b border-clay_dark focus:outline-none placeholder-clay_dark" 
-                placeholder="Search Products" 
+            <input
+                type="text"
+                id="search_input"
+                className="text-center bg-transparent border-b border-clay_dark focus:outline-none placeholder-clay_dark"
+                placeholder="Search Products"
                 required
             />
         </div>
