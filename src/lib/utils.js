cddaa323a31ba567b75ba8b07a7d0ff4ee98c149
Pirouz Mehmandoost <pirouz.sf@gmail.com>
2025-01-16 18:58:42 -0800

    Move functions to lib/utils.js

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -7,12 +7,12 @@ export function flattenAttributes(data) {
         typeof data === "function"
     ) {
         return data;
-    }
+    };
 
     // If data is an array, apply flattenAttributes to each element and return as array
     if (Array.isArray(data)) {
         return data.map((item) => flattenAttributes(item));
-    }
+    };
 
     // Initialize an object with an index signature for the flattened structure
     let flattened = {};
@@ -33,7 +33,40 @@ export function flattenAttributes(data) {
             // For other keys, copy the value, applying flattenAttributes if it's an object
             flattened[key] = flattenAttributes(data[key]);
         }
-    }
-
+    };
     return flattened;
-}
+};
+
+
+export const scaleMeshAtBreakpoint = (width) => {
+    if (width <= 360) {
+        return 0.6;
+    }
+    if (width <= 480) {
+        return 0.7;
+    }
+    if (width <= 640) { //sm tailwind breakpoint
+        return 0.8;
+    }
+    if (width <= 768) { //md  tailwind breakpoint
+        return 0.9;
+    }
+    return 1;   //lg, xl, 2xl tailwind breakpoints 
+};
+
+
+export const scaleXAtBreakPoint = (width) => {
+    if (width <= 360) {
+        return 1.5;
+    }
+    if (width <= 480) {
+        return 1.2;
+    }
+    if (width <= 640) { //sm tailwind breakpoint
+        return 1.0;
+    }
+    if (width <= 768) { //md tailwind breakpoint
+        return 0.9;
+    }
+    return 0.7; //lg, xl, 2xl tailwind breakpoints
+};
