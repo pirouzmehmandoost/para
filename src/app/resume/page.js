d19affcb0c495a243e444880919d864a129a9f02
Pirouz Mehmandoost <pirouz.sf@gmail.com>
2025-01-19 14:37:00 -0800

    restyle resume page

--- src/app/resume/page.js
+++ src/app/resume/page.js
@@ -2,14 +2,16 @@
 
 export default function Resume() {
   return (
-    <div className="flex flex-col w-screen min-w-screen h-screen min-h-screen">
-      <iframe
-        className="mt-28"
-        src={"/pirouz_mehmandoost_swe_resume.pdf"}
-        width="100%"
-        height="100%"
-        allowFullScreen
-      />
+    <div className="mt-28  mb-20  flex flex-col w-full min-w-full h-full min-h-full items-center justify-stretch">
+      <div className="flex flex-col w-5/6 h-screen min-h-screen">
+        <iframe
+          className="h-full place-self-center items-center place-items-center"
+          src={"/pirouz_mehmandoost_swe_resume.pdf"}
+          width="90%"
+          height="80%"
+          allowFullScreen
+        />
+      </div>
     </div>
   );
 }
