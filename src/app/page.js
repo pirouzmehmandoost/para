981f62e675e08879d8e05190ed8b8591c7f372ba
Pirouz Mehmandoost <pirouz.sf@gmail.com>
2025-01-20 15:54:45 -0800

    edit text on home page

--- src/app/page.js
+++ src/app/page.js
@@ -23,10 +23,8 @@ export default function Home() {
         className={`flex flex-col basis-7/12 w-5/6 h-fit mt-36 place-self-center text-3xl sm:text-3xl md:text-3xl lg:text-4xl xl:text-5xl uppercase ${myFont.className}`}
       >
         <div className="w-full my-4">
-          <p>Hey There! I&apos;m a software engineer based out of the San Francisco Bay Area.</p>
-          <p>
-            I specialize in frontend development with JavaScript Frameworks. I&apos;m
-            also a designer with passions for 3D computer graphics and additive manufacturing.
+          <p>Hey There! I&apos;m a software engineer based in the San Francisco Bay Area. I specialize in frontend development with JavaScript Frameworks.
+            I&apos;m also a designer with passions for 3D computer graphics and additive manufacturing.
           </p>
           {/* <p className="mt-10">
             Check the github repo and my development notes while I continue working on this app ☺
@@ -38,7 +36,7 @@ export default function Home() {
         className="flex flex-col basis-5/12"
       >
         <h1
-          className={`my-12 text-4xl sm:text-5xl md:text-6xl lg:text-6xl xl:text-6xl italic ${myFont.className}`}
+          className={`mt-12 mb-16 text-4xl sm:text-5xl md:text-6xl lg:text-6xl xl:text-6xl italic ${myFont.className}`}
         >
           Recent Projects
         </h1>
