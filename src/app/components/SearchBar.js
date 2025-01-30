7e1562273d669f27fd8edaf79d742aaf6f2d7598
Pirouz Mehmandoost <pirouz.sf@gmail.com>
2025-01-29 17:15:55 -0800

    update styling in SearchBar

--- src/app/components/SearchBar.js
+++ src/app/components/SearchBar.js
@@ -6,7 +6,7 @@ const SearchBar = () => {
     // const [searchQuery, setsearchQuery] = useState("invisible");
 
     return (
-        <div className="flex flex-row flex-grow grid text-center text-2xl text-clay_dark" >
+        <div className="flex flex-row flex-grow text-center text-2xl text-clay_dark" >
             <input
                 type="text"
                 id="search_input"
