82d2a3ff1f2bc24009e8ac2ade7644fb684d162a
Pirouz Mehmandoost <pirouz.sf@gmail.com>
2025-02-11 23:37:37 -0800

    change margins and basis styling

--- src/app/page.js
+++ src/app/page.js
@@ -10,30 +10,35 @@ const myFont = localFont({
 
 export default function Home() {
   return (
-    <main className="flex flex-col w-screen min-w-screen h-screen min-h-screen text-center text-clay_dark">
+    <main className="flex flex-col w-screen min-w-screen h-full min-h-screen text-center text-clay_dark">
       <div
         id="top_section"
-        className={`flex flex-col basis-7/12 w-5/6 h-fit mt-36 place-self-center text-3xl sm:text-3xl md:text-3xl lg:text-4xl xl:text-5xl uppercase ${myFont.className}`}
+        className={`flex flex-col basis-5/12 w-5/6 h-fit mt-36 place-self-center text-3xl sm:text-3xl md:text-3xl lg:text-4xl xl:text-5xl uppercase ${myFont.className}`}
       >
         <div className="w-full my-4">
           <p>Hey There! I&apos;m a software engineer based in the San Francisco Bay Area. I specialize in frontend development with JavaScript Frameworks.
             I&apos;m also a designer with passions for interactive design, 3D computer graphics and 3D printing.
           </p>
+          <p
+            className={`mt-14 text-4xl sm:text-5xl md:text-6xl lg:text-6xl xl:text-6xl italic ${myFont.className}`}
+          >
+            Recent Projects
+          </p>
         </div>
+
       </div>
+      {/* end top section */}
+
       <div
         id="bottom_section"
-        className="flex flex-col basis-5/12"
+        className="flex flex-col basis-7/12 mt-12"
       >
-        <h1
-          className={`mt-12 mb-16 text-4xl sm:text-5xl md:text-6xl lg:text-6xl xl:text-6xl italic ${myFont.className}`}
-        >
-          Recent Projects
-        </h1>
-        <div className="my-10 w-full h-full ">
+        <div className="w-full h-full ">
           <ProjectBanner />
         </div>
       </div>
+      {/* end top bottom */}
+
     </main>
   );
 }
