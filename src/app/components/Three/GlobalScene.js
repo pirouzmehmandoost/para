9f3ca7d6471ff1fa2b82a3316f65f495984ce809
Pirouz Mehmandoost <pirouz.sf@gmail.com>
2025-02-13 19:19:11 -0800

    remove unused files

--- src/app/components/Three/GlobalScene.js
+++ src/app/components/Three/GlobalScene.js
@@ -167,3 +167,157 @@ export const GlobalScene = () => {
 };
 
 export default GlobalScene;
+
+
+
+// const Scene = (data) => {
+//   const router = useRouter()
+//   const [modelPosition, setModelPosition] = useState([]);
+//   const { size } = useThree();
+//   const groupRef = useRef();
+//   const setSelection = useSelection((state) => state.setSelection);
+
+//   console.log("Scene data: ", data)
+//   const {
+//     colorCodes,
+//     modelUrls,
+//     autoRotate,
+//     scale,
+//     position: groupPosition,
+//     data: selectedProject,
+//   } = data;
+
+//   const positions = [];
+//   // let groupScale = scaleMeshAtBreakpoint(size.width);
+//   const handles = [];
+//   const bezierCurve = new THREE.EllipseCurve(
+//     0,
+//     0,
+//     40,
+//     30,
+//     0,
+//     2 * Math.PI,
+//     false,
+//     0
+//   );
+//   bezierCurve.closed = true;
+
+//   const bezierCurvePoints = bezierCurve.getPoints(modelUrls.length);
+//   bezierCurvePoints.shift(); //remove an overlapping point
+
+//   const bezierGeometry = new THREE.BufferGeometry().setFromPoints(bezierCurvePoints);
+
+//   const ellipse = new THREE.Line(bezierGeometry, new THREE.MeshBasicMaterial());
+//   ellipse.rotation.x = Math.PI * 0.5;
+
+//   const vertex = new THREE.Vector3();
+//   const positionAttribute = ellipse.geometry.getAttribute('position');
+
+//   const handlePositions = [];
+
+//   for (let vertexIndex = 0; vertexIndex < positionAttribute.count; vertexIndex++) {
+//     const pt = vertex.fromBufferAttribute(positionAttribute, vertexIndex);
+
+//     handlePositions.push({
+//       x: pt.x,
+//       y: 0,
+//       z: pt.y
+//     });
+//   };
+
+//   const boxGeometry = new THREE.BoxGeometry(2, 2, 2); // to visualize handle positions
+//   // const boxMaterial = new THREE.MeshBasicMaterial({ color: "red" });
+//   for (const handlePosition of handlePositions) {
+//     const handle = new THREE.Mesh(boxGeometry)//, boxMaterial);
+//     handle.position.copy(handlePosition);
+//     handles.push(handle);
+//     // scene.add(handle);
+//   }
+//   // scene.add(ellipse)
+//   // const material = new THREE.MeshPhysicalMaterial({ ...colorCodes.defaultColor.material })
+//   // material.wireframe = true
+//   // material.vertexColors = true
+
+//   // const cameraPathCurve = new THREE.CatmullRomCurve3(handles.map((handle) => handle.position));
+//   // cameraPathCurve.curveType = 'centripetal';
+//   // cameraPathCurve.closed = true;
+
+//   // const points = cameraPathCurve.getPoints(modelUrls.length);
+//   // const line = new THREE.LineLoop(
+//   //   new THREE.BufferGeometry().setFromPoints(points),
+//   //   new THREE.LineBasicMaterial({ color: 0x00ff00 })
+//   // );
+//   // scene.add(line);
+
+//   useEffect(() => {
+//     // groupScale = scaleMeshAtBreakpoint(size.width);
+//     if (modelUrls.length > 1) {
+//       for (const handlePosition of handlePositions) {
+//         const p = [
+//           handlePosition.x * 1.6,
+//           handlePosition.y,
+//           handlePosition.z * 1.5,
+//         ]
+//         positions.push(p);
+//       }
+//       setModelPosition(positions);
+//       // if (groupRef.current) {
+//       //  groupRef.current.scale.set(groupScale, groupScale, groupScale);
+//       // }
+//     };
+
+//   }, [modelUrls]); //three.js state is not included in dependency array
+
+//   // Update camera position and orbit controls 
+//   // useFrame(({ clock }) => {
+//   // let s = (clock.getElapsedTime() * 0.1) % 1;
+
+//   // if (groupRef.current) {
+//   // const position = cameraPathCurve.getPoint(s);
+//   // groupRef.current.position.copy(position);
+//   // groupRef.current.scale.set(groupScale, groupScale, groupScale);
+
+//   // if (modelUrls.length > 1) {
+//   //   camera.position.x = position.x;
+//   //   camera.position.y = modelUrls.length > 2 ? cameraPosition[1] + 27 : cameraPosition[1];
+//   //   camera.position.z = position.z + cameraPosition[2];
+//   //   camera.lookAt(position);
+//   // } else {
+//   //   camera.position.x = 0
+//   //   camera.position.y = cameraPosition[1];
+//   //   camera.position.z = cameraPosition[2]
+//   //   camera.lookAt(0, 0, 0);
+//   // }
+//   // };
+//   // });
+
+//   return (
+//     <group
+//       ref={groupRef}
+//       position={groupPosition}
+//       onClick={() => {
+//         console.log("clicked!", selectedProject.name);
+//         setSelection(selectedProject);
+//         router.push('/project');
+//       }}
+//     >
+//       {
+//         modelUrls.map((url, index) => {
+//           const updateScale = modelUrls.length === 1 ? scale * 0.5 : scaleMeshAtBreakpoint(size.width) / modelUrls.length;
+//           const newProps = {
+//             modelUrl: url,
+//             material: modelUrls.length === 1 ? { ...colorCodes.defaultColor.material } : Object.values(colorCodes.colorWays)[index].material, // material properties
+//             scale: updateScale,
+//             autoRotate: modelUrls.length === 1 || index > 0 ? autoRotate : false,
+//             position: modelPosition[index]
+//           };
+
+//           return (
+//             <Model key={index} {...newProps} />
+//           );
+//         })
+//       }
+//     </group>
+//   );
+// };
+
