977dd838e13829eccce86c661fe558628be47e0c
Pirouz Mehmandoost <pirouz.sf@gmail.com>
2025-03-12 17:58:25 -0700

    begin work on BottomMenu component

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 'use client';
 
 import Link from 'next/link';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { AnimatePresence, motion } from 'framer-motion';
 import { GlobalModelViewer } from '@/components/Three/GlobalModelViewer';
 import MenuIcon from '@mui/icons-material/Menu';
@@ -298,14 +298,278 @@ const Menu = () => {
   );
 };
 
+const BottomMenu = ({ showMenu }) => {
+  const motionVariants = {
+    top: {
+      initial: {
+        filter: 'blur(100px)',
+        opacity: 1,
+        rotateX: 90,
+        translateX: -20,
+        translateY: 80,
+      },
+      enter: (i) => ({
+        filter: 'blur(0px)',
+        opacity: 1,
+        rotateX: 0,
+        translateX: 0,
+        translateY: 0,
+        transition: {
+          duration: 0.65,
+          delay: 0.5 + i * 0.1,
+          ease: [0.215, 0.61, 0.355, 1],
+          opacity: { duration: 0.35 },
+        },
+      }),
+      exit: (i) => ({
+        filter: 'blur(500px)',
+        opacity: 0,
+        transition: {
+          duration: 0.5,
+          delay: 0.5 - i * 0.1,
+          type: 'easeInOut',
+          ease: [0.76, 0, 0.24, 1],
+        },
+      }),
+    },
+    bottom: {
+      initial: {
+        filter: 'blur(100px)',
+        opacity: 1,
+        y: 20,
+      },
+      enter: (i) => ({
+        filter: 'blur(0px)',
+        opacity: 1,
+        y: 0,
+        transition: {
+          duration: 0.5,
+          delay: 0.75 + i * 0.1,
+          ease: [0.215, 0.61, 0.355, 1],
+        },
+      }),
+      exit: {
+        filter: 'blur(100px)',
+        opacity: 0,
+        transition: { duration: 0.5, type: 'tween', ease: 'easeInOut' },
+      },
+    },
+    menu: {
+      open: {
+        filter: 'blur(0px)',
+        opacity: 1,
+        width: 'auto',
+        height: '25vh',
+        // top: '-25px',
+        // right: '-25px',
+        // boxShadow: '0px -300px 80px 100px rgba(255,255,255,.3)',
+        transition: {
+          duration: 0.75,
+          type: 'tween',
+          ease: [0.76, 0, 0.24, 1],
+          opacity: { delay: 0.5 },
+        },
+      },
+      closed: {
+        filter: 'blur(500px)',
+        opacity: 0,
+        width: '1vw',
+        height: '1vh',
+        // top: '0px',
+        // right: '0px',
+        // top: '50vh',
+        // right: '50vw',
+        transition: {
+          delay: 0.5,
+          duration: 0.75,
+          type: 'easeInOut',
+          ease: [0.76, 0, 0.24, 1],
+          opacity: { delay: 0.5 },
+        },
+      },
+    },
+    overlay: {
+      open: {
+        // backdropFilter: 'blur(100px)',
+        // filter: 'blur(10px)',
+        opacity: 1,
+        width: '100vw',
+        height: '100vh',
+        transition: {
+          delay: 0,
+          duration: 0.5,
+          type: 'easeInOut',
+          ease: [0.76, 0, 0.24, 1],
+          opacity: { delay: 0.5 },
+          // backdropFilter: { delay: 0 },
+        },
+      },
+      closed: {
+        // backdropFilter: 'blur(0px)',
+        // filter: 'blur(0px)',
+        opacity: 0,
+        width: '1vw',
+        height: '1vh',
+        transition: {
+          delay: 0.5,
+          duration: 0.6,
+          type: 'easeInOut',
+          ease: [0.76, 0, 0.24, 1],
+          opacity: { delay: 0.5 },
+          // backdropFilter: { delay: 0 },
+        },
+      },
+    },
+  };
+
+  const [isActive, setIsActive] = useState(showMenu);
+
+  console.log('BottomMenu. showMenu: ', showMenu);
+
+  useEffect(() => {
+    if (showMenu) setIsActive(showMenu);
+    else setIsActive(undefined);
+  }, [showMenu]);
+
+  const links = [
+    {
+      title: 'Close Me',
+      callBack: true,
+    },
+
+    {
+      title: `${isActive?.current?.name}`,
+      callBack: false,
+    },
+  ];
+
+  const MenuLinks = ({ callBack }) => {
+    return (
+      <div className="flex flex-col w-full h-full justify-around text-center text-4xl uppercase text-neutral-900 bg-neutral-300">
+        {/* Header */}
+        <motion.div
+          key={`b_0`}
+          custom={0}
+          variants={motionVariants.top}
+          initial="initial"
+          animate="enter"
+          exit="exit"
+          className="flex flex-row w-full h-fit justify-center text-nowrap perspective-origin-bottom bg-neutral-500/0"
+          style={{
+            maskImage:
+              'radial-gradient(ellipse 80% 100% at 50% 50% , #a3a3a3 30%, #a3a3a300 90%)',
+          }}
+        >
+          <div className="text-nowrap sm:text-4xl md:text-4xl lg:text:5xl xl:text-5xl 2xl:text-5xl md:px-1 xl:px-1 lg:px-1 2xl:px-1">
+            Some Text
+          </div>
+        </motion.div>
+        {/* top links */}
+        <div
+          className="flex flex-row w-full h-fit justify-between bg-neutral-500/0"
+          style={{
+            maskImage:
+              'radial-gradient(ellipse 70% 80% at 50% 50% , #a3a3a3 30%, #a3a3a300 70%)',
+          }}
+        >
+          {links.map((link, i) => {
+            const { title, href } = link;
+            return (
+              <div
+                key={`b_${i + 1}`}
+                className="perspective-origin-bottom my-2"
+              >
+                <motion.div
+                  custom={i + 1}
+                  variants={motionVariants.top}
+                  initial="initial"
+                  animate="enter"
+                  exit="exit"
+                >
+                  {link.callBack ? (
+                    <div className="cursor-pointer" onClick={() => callBack()}>
+                      {title}
+                    </div>
+                  ) : (
+                    <p>{title} </p>
+                  )}
+                </motion.div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  };
+
+  // const CloseButton = ({ isActive, callBack }) => {
+  //   return (
+  //     <div
+  //       onClick={() => callBack()}
+  //       className={`fixed w-fit h-fit inset-0 bottom-5 left-5 justify-center p-4 rounded-full cursor-pointer bg-red-500 backdrop-blur-xl transition-all duration-1000 ease-in-out text-neutral-700 hover:text-neutral-700  ${isActive ? 'opacity-0 w-1 h-1  bg-neutral-500/30' : 'opacity-100 w-fit h-fit'}`}
+  //     >
+  //       <MenuIcon fontSize="large" />
+  //     </div>
+  //   );
+  // };
+
+  return (
+    <div
+      className="relative flex flex-row grow w-full h-full z-1 justify-center bg-neutral-500/0"
+      style={{
+        maskImage:
+          'radial-gradient(ellipse 70% 80% at 50% 50% , #a3a3a3 30%, #a3a3a300 70%)',
+      }}
+    >
+      {/* <CloseButton
+        isActive={isActive}
+        callBack={() => {
+          setIsActive(!isActive);
+        }}
+      /> */}
+      {/* menu/links container */}
+      <motion.div
+        variants={motionVariants.menu}
+        animate={isActive ? 'open' : 'closed'}
+        initial="open"
+      >
+        <AnimatePresence>
+          {isActive && (
+            <div className="bg-neutral-300/50 text-4xl uppercase text-neutral-900 backdrop-blur-xl ">
+              {isActive ? isActive?.current?.name : 'nothing selected'}
+            </div>
+            // <MenuLinks
+            //   callBack={() => {
+            //     setIsActive(!isActive);
+            //   }}
+            // />
+          )}
+        </AnimatePresence>
+      </motion.div>
+      {/* overlay a blurry div */}
+      {/* <motion.div
+        className="absolute flex flex-col grow w-full h-1/2 -z-1 place-self-center justify-center bg-pink-400/100 backdrop-blur-xl"
+        variants={variants.overlay}
+        animate={isActive ? 'open' : 'closed'}
+        initial="open"
+      /> */}
+    </div>
+  );
+};
+
 const Home = () => {
+  const [visible, setVisible] = useState(null);
+
   return (
     <main className="flex flex-col w-full h-full">
       <div className="relative flex w-full h-fit">
         <Menu />
       </div>
-      <div className="fixed inset-0 bottom-10 flex-col grow w-full h-full">
-        <GlobalModelViewer />
+      <div className="fixed inset-0 bottom-10 flex flex-col grow w-full h-full">
+        <GlobalModelViewer showMenu={setVisible} />
+      </div>
+      <div className="fixed bottom-0 flex flex-col grow w-full h-1/4 bg-yellow-400/0 ">
+        <BottomMenu showMenu={visible} />
       </div>
     </main>
   );
