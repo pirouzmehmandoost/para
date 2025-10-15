d335bd5043219698f88058d1dabc642ceda65bcd
pirouzmehmandoost <pirouzmehmandoost@gmail.com>
2025-10-15 00:56:13 -0700

    remove unused props

--- src/app/components/three/groups/Group.js
+++ src/app/components/three/groups/Group.js
@@ -7,48 +7,52 @@ import Model from '../models/Model';
 const Group = (data) => {
   const { size } = useThree();
   const {
-    autoRotate,
-    autoRotateSpeed,
     materials,
     modelUrls,
-    isPointerOver,
     position: groupPosition,
     scale,
   } = data;
 
-  let positions = calculatePositions(
-    size.width,
-    modelUrls.length,
-    groupPosition,
-  );
-
-  let updateScale =
-    modelUrls.length === 1
-      ? scale * 0.45
-      : scaleMeshAtBreakpoint(size.width) / modelUrls.length;
+  const positions = calculatePositions(size.width, modelUrls.length, groupPosition);
+  const modelScale = modelUrls.length === 1 
+    ? scale * 0.45 
+    : scaleMeshAtBreakpoint(size.width) / modelUrls.length;
 
   return (
     <>
-      {modelUrls.map((modelUrl, index) => {
-        const key = modelUrl.name;
-        const props = {
-          ...data,
-          autoRotate: autoRotate,
-          autoRotateSpeed: autoRotateSpeed,
-          isPointerOver,
-          materialId:
+      {modelUrls.map((modelUrl, index) => (
+        <Model
+          key={modelUrl.name}
+          {...data}
+          materialId={
             modelUrls.length === 1
               ? materials.defaultMaterial
-              : Object.values(materials.colorWays)[index],
-          modelUrl: modelUrl,
-          position: positions[index],
-          scale: updateScale,
-        };
-
-        return <Model key={key} {...props} />;
-      })}
+              : Object.values(materials.colorWays)[index]
+          }
+          modelUrl={modelUrl}
+          position={positions[index]}
+          scale={modelScale}
+        />
+      ))}
     </>
-  );
+    );
+    //   {modelUrls.map((modelUrl, index) => {
+    //     const key = modelUrl.name;
+    //     const props = {
+    //       ...data,
+    //       autoRotate: autoRotate,
+    //       autoRotateSpeed: autoRotateSpeed,
+    //       isPointerOver,
+    //       materialId: modelUrls.length === 1 ? materials.defaultMaterial : Object.values(materials.colorWays)[index],
+    //       modelUrl: modelUrl,
+    //       position: positions[index],
+    //       scale: modelScale,
+    //     };
+
+    //     return <Model key={key} {...props} />;
+    //   })}
+    // </>
+
 };
 
 export default Group;
