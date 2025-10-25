0778b8cedb9d0688c06ad70f137b8f014b3a2c91
pirouzmehmandoost <pirouzmehmandoost@gmail.com>
2025-10-25 16:55:29 -0700

    remove comments

--- src/app/components/three/groups/Group.js
+++ src/app/components/three/groups/Group.js
@@ -1,22 +1,17 @@
 'use client';
 
 import { useThree } from '@react-three/fiber';
-import { scaleMeshAtBreakpoint, calculatePositions } from '@utils/mesh/meshUtils';
+import { calculatePositions, scaleMeshAtBreakpoint } from '@utils/mesh/meshUtils';
 import Model from '../models/Model';
 
 const Group = (data) => {
   const { size } = useThree();
-  const {
-    materials,
-    modelUrls,
-    position: groupPosition,
-    scale,
-  } = data;
-
+  const { materials, modelUrls, position: groupPosition, scale } = data;
   const positions = calculatePositions(size.width, modelUrls.length, groupPosition);
+  
   const modelScale = modelUrls.length === 1 
     ? scale * 0.45 
-    : scaleMeshAtBreakpoint(size.width) / modelUrls.length;
+    : ( scaleMeshAtBreakpoint(size.width) / modelUrls.length );
 
   return (
     <>
@@ -25,8 +20,8 @@ const Group = (data) => {
           key={modelUrl.name}
           {...data}
           materialId={
-            modelUrls.length === 1
-              ? materials.defaultMaterial
+            modelUrls.length === 1 
+              ? materials.defaultMaterial 
               : Object.values(materials.colorWays)[index]
           }
           modelUrl={modelUrl}
@@ -35,24 +30,7 @@ const Group = (data) => {
         />
       ))}
     </>
-    );
-    //   {modelUrls.map((modelUrl, index) => {
-    //     const key = modelUrl.name;
-    //     const props = {
-    //       ...data,
-    //       autoRotate: autoRotate,
-    //       autoRotateSpeed: autoRotateSpeed,
-    //       isPointerOver,
-    //       materialId: modelUrls.length === 1 ? materials.defaultMaterial : Object.values(materials.colorWays)[index],
-    //       modelUrl: modelUrl,
-    //       position: positions[index],
-    //       scale: modelScale,
-    //     };
-
-    //     return <Model key={key} {...props} />;
-    //   })}
-    // </>
-
+  );
 };
 
 export default Group;
