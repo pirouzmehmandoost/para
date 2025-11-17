e5389c38a365281e589d0d4fba186ab9d8463172
pirouzmehmandoost <pirouzmehmandoost@gmail.com>
2025-11-17 15:10:22 -0800

    Add vercel speed insights, and change background color

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import './globals.css';
 import localFont from 'next/font/local';
 import { SpeedInsights } from "@vercel/speed-insights/next"
-
+import { envColor } from '@configs/globals';
 
 const myFont = localFont({
   src: './../../public/fonts/halibutSerif/web/HalibutSerif-Condensed.woff2',
@@ -16,7 +16,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${myFont.className} bg-cover bg-[#bcbcbc]`}>
+      <body className={`${myFont.className} bg-cover bg-[${envColor}]`}>
         {children}
         <SpeedInsights />
       </body>
