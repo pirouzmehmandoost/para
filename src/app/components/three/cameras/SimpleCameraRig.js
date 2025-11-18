1853fa4565a57162053c8af30541bf00da3a8525
pirouzmehmandoost <pirouzmehmandoost@gmail.com>
2025-11-18 15:03:38 -0800

    rename variable v->positionVector and memoize it

--- src/app/components/three/cameras/SimpleCameraRig.js
+++ src/app/components/three/cameras/SimpleCameraRig.js
@@ -1,19 +1,26 @@
 'use client';
 
-import { Vector3 } from 'three';
+import * as THREE from 'three';
 import { useFrame } from '@react-three/fiber';
+import { useMemo } from 'react'
 
-const SimpleCameraRig = (data) => {
-    const { cameraPosition = [0, 10, 180] } = data;
-    const v = new Vector3();
+THREE.Cache.enabled = true;
+
+const SimpleCameraRig = (props) => {
+    const { 
+      cameraPosition = [0, 10, 180],
+    } = props;
+
+    const positionVector = useMemo(() => new THREE.Vector3(), []);
   
-    return useFrame(({ clock, camera }) => {
+    useFrame(({ clock, camera }) => {
       const t = clock.elapsedTime;
       const sin = Math.sin(t/2);
       const cos = Math.cos(t/2);
-  
-      camera.position.lerp( v.set(0, (cos * 100), (sin + 1) * cameraPosition[2]), 0.06);
-      camera.lookAt(0, 0, 0);
+
+      positionVector.set(sin, (cos*100), (sin+1)*cameraPosition[2]);
+      camera.position.lerp(positionVector, 0.06);
+      camera.lookAt(0,0,0);
     });
   };
 
