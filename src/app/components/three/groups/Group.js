2daa36cd75b053d21e0460073d0f662bb059555d
pirouzmehmandoost <pirouzmehmandoost@gmail.com>
2025-11-17 18:03:05 -0800

    utilize new props onMeshReafy and groundMeshRef

--- src/app/components/three/groups/Group.js
+++ src/app/components/three/groups/Group.js
@@ -1,35 +1,41 @@
 'use client';
 
 import { useThree } from '@react-three/fiber';
-import { calculatePositions, scaleMeshAtBreakpoint } from '@utils/mesh/meshUtils';
+import { calculateXZPositions, scaleMeshAtBreakpoint } from '@utils/mesh/meshUtils';
 import Model from '../models/Model';
 
 const Group = (data) => {
   const { size } = useThree();
-  const { materials, modelUrls, position: groupPosition, scale } = data;
-  const positions = calculatePositions(size.width, modelUrls.length, groupPosition);
-  
-  const modelScale = modelUrls.length === 1 
-    ? scale * 0.45 
-    : ( scaleMeshAtBreakpoint(size.width) / modelUrls.length );
+  const {
+    materials,
+    modelUrls,
+    groundMeshRef,
+    groupRef,
+    onMeshReady,
+    position: groupPosition,
+    scale,
+    ...rest
+  } = data;
 
+  const positions = calculateXZPositions(size.width, modelUrls.length, groupPosition);
+  const modelScale = modelUrls.length === 1 ? scale * 0.45 : (scaleMeshAtBreakpoint(size.width)/modelUrls.length);
+  
   return (
-    <>
-      {modelUrls.map((modelUrl, index) => (
-        <Model
-          key={modelUrl.name}
-          {...data}
-          materialId={
-            modelUrls.length === 1 
-              ? materials.defaultMaterial 
-              : Object.values(materials.colorWays)[index]
-          }
-          modelUrl={modelUrl}
-          position={positions[index]}
-          scale={modelScale}
-        />
-      ))}
-    </>
+    <group ref={groupRef} {...rest}>
+      {
+        modelUrls.map((modelUrl, index) => (
+          <Model
+            onMeshReady={onMeshReady[index]}
+            key={modelUrl.name}
+            groundMeshRef={groundMeshRef}
+            materialId={ modelUrls.length === 1 ? materials.defaultMaterial : Object.values(materials.colorWays)[index] }
+            modelUrl={modelUrl}
+            position={positions[index]}
+            scale={modelScale}
+          />
+        ))
+      }
+    </group>
   );
 };
 
