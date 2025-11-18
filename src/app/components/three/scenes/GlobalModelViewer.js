cf2735499afe37fd6b65e1a38666d1e9754d8441
pirouzmehmandoost <pirouzmehmandoost@gmail.com>
2025-11-17 18:15:19 -0800

    remove directionalLights, add suspense fallback

--- src/app/components/three/scenes/GlobalModelViewer.js
+++ src/app/components/three/scenes/GlobalModelViewer.js
@@ -1,21 +1,21 @@
 'use client';
 
-import { Suspense } from 'react';
+import {Suspense } from 'react';
 import * as THREE from 'three';
 import { Canvas } from '@react-three/fiber';
-import { Environment, AdaptiveDpr, Html, PerformanceMonitor } from '@react-three/drei';
+import { AdaptiveDpr, Environment, Html} from '@react-three/drei';
 import cameraConfigs from '@configs/cameraConfigs';
-import { groundColor, backgroundImageURL } from '@configs/globals';
-import Ground from '../models/Ground';
+import { envColor, envImageUrl } from '@configs/globals';
 import SceneBuilder from './SceneBuilder';
 
 THREE.ColorManagement.enabled = true;
+THREE.Cache.enabled = true;
 
 export const GlobalModelViewer = ({ showMenu }) => {
   return (
     <Canvas
       camera={{
-        position: [666, 80, 666],
+        position: [666, 666, 666],
         near: cameraConfigs.NEAR,
         far: cameraConfigs.FAR,
         fov: 50,
@@ -24,60 +24,39 @@ export const GlobalModelViewer = ({ showMenu }) => {
       orthographic={false}
       shadows
     >
-      <AdaptiveDpr pixelated />
-      <Environment shadows files={backgroundImageURL} />
-      <color args={[groundColor]} attach="background" />
-      <fog attach="fog" density={0.006} color={groundColor} near={150} far={280} />
-      <directionalLight
-        castShadow={true}
-        position={[0, 80, -40]}
-        shadow-mapSize-width={1024}
-        shadow-mapSize-height={1024}
-        intensity={1}
-        shadow-camera-near={0.05}
-        shadow-camera-far={1000}
-        shadow-bias={-0.001}
-        shadow-camera-top={1500}
-        shadow-camera-bottom={-1500}
-        shadow-camera-left={-1500}
-        shadow-camera-right={1500}
-      />
-      <directionalLight
-        castShadow={true}
-        position={[0, 100, 80]}
-        shadow-mapSize-width={1024}
-        shadow-mapSize-height={1024}
-        intensity={1}
-        shadow-camera-near={0.05}
-        shadow-camera-far={1000}
-        shadow-bias={-0.001}
-        shadow-camera-top={1500}
-        shadow-camera-bottom={-1500}
-        shadow-camera-left={-1500}
-        shadow-camera-right={1500}
-      />
-      {/* <Suspense 
-        fallback={
-          <Html center>
-            <div style={{
-              color: 'white',
-              fontSize: '20px',
-              fontFamily: 'system-ui',
-              textAlign: 'center',
-              padding: '20px'
-            }}>
-              Loading 3D Scene...
-            </div>
-          </Html>
-        }
-      > */}
-        <SceneBuilder showMenu={showMenu} />
-      {/* </Suspense> */}
-      <Ground
-        position={[-50, -85, 20]}
-        scale={[1.4, 1, 1.4]}
-        rotation={Math.PI / 7}
-      />
+      <Suspense fallback={
+        <Html
+          center 
+          position={[0, 0, 0]}
+          scale={[40, 40, 40]}
+          style={{
+            color: 'black',
+            fontSize: '100px',
+            fontFamily: 'didot',
+            textAlign: 'center',
+          }}>
+            Loading...
+        </Html>
+      }>
+        <AdaptiveDpr pixelated />
+        <Environment shadows files={envImageUrl} />
+        <color args={[envColor]} attach="background" />
+        <fog
+          attach="fog" color={envColor} density={0.005} near={180} far={280}/>
+        {/* <Clouds material={THREE.MeshLambertMaterial} limit={400}>
+          <Cloud
+            concentrate="outside"
+            growth={200}
+            color="white"
+            opacity={0.1}
+            seed={0.3}
+            bounds={400}
+            volume={300}
+            position={[80,80,0]}
+          />
+        </Clouds> */}
+          <SceneBuilder showMenu={showMenu}/>
+      </Suspense>
     </Canvas>
   );
 };
