48d912617effe9c9a19da709b20b6de31e8dbc00
pirouzmehmandoost <pirouzmehmandoost@gmail.com>
2025-11-19 02:06:52 -0800

    Add clouds

--- src/app/components/three/scenes/GlobalModelViewer.js
+++ src/app/components/three/scenes/GlobalModelViewer.js
@@ -3,7 +3,7 @@
 import {Suspense } from 'react';
 import * as THREE from 'three';
 import { Canvas } from '@react-three/fiber';
-import { AdaptiveDpr, Environment, Html} from '@react-three/drei';
+import { AdaptiveDpr, Clouds, Cloud, Environment, Html} from '@react-three/drei';
 import cameraConfigs from '@configs/cameraConfigs';
 import { envColor, envImageUrl } from '@configs/globals';
 import SceneBuilder from './SceneBuilder';
@@ -41,8 +41,49 @@ export const GlobalModelViewer = ({ showMenu }) => {
         <AdaptiveDpr pixelated />
         <Environment shadows files={envImageUrl} />
         <color args={[envColor]} attach="background" />
-        <fog attach="fog" color={envColor} density={0.005} near={180} far={280} />
-          <SceneBuilder showMenu={showMenu}/>
+        <fog attach="fog" color={envColor} density={0.004} near={180} far={290} />
+        <Clouds material={THREE.MeshLambertMaterial} limit={100}>
+          <Cloud
+            bounds={[20, 20, 20]}
+            color='white'
+            concentrate='random'
+            fade={10}
+            growth={40}
+            opacity={0.1}
+            position={[350,-10,0]}
+            seed={0.4}
+            segments={10}
+            speed={0.3}
+            volume={400}
+          />
+          <Cloud
+            bounds={[20, 20, 20]}
+            color='white'
+            concentrate='random'
+            fade={10}
+            growth={40}
+            opacity={0.1}
+            position={[-350,-10,0]}
+            seed={0.4}
+            segments={10}
+            speed={0.3}
+            volume={400}
+          />
+          <Cloud
+            bounds={[20, 20, 20]}
+            color='white'
+            concentrate='random'
+            fade={10}
+            growth={40}
+            opacity={0.1}
+            position={[0,0,130]}
+            seed={0.4}
+            segments={10}
+            speed={0.3}
+            volume={400}
+          />
+        </Clouds>
+        <SceneBuilder showMenu={showMenu}/>
       </Suspense>
     </Canvas>
   );
