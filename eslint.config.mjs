69d7ea07ea9eeef60239c4f9739d2250519a6380
pirouzmehmandoost <pirouzmehmandoost@gmail.com>
2025-12-12 01:40:11 -0800

    Update dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,38 @@
-/** @type {import('eslint').Linter.Config[]} */
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
 
-import { FlatCompat } from "@eslint/eslintrc";
-import path from "path";
-import { fileURLToPath } from "url";
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  globalIgnores([
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+  ]),
+])
+ 
+export default eslintConfig;
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// /** @type {import('eslint').Linter.Config[]} */
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+// import { FlatCompat } from "@eslint/eslintrc";
+// import path from "path";
+// import { fileURLToPath } from "url";
 
-const configs = [
-  ...compat.extends("next/core-web-vitals"),
-  {
-    rules: {
-      "prefer-const": "warn",
-    }
-  },
-];
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
-export default configs;
+// const compat = new FlatCompat({
+//   baseDirectory: __dirname,
+// });
+
+// const configs = [
+//   ...compat.extends("next/core-web-vitals"),
+//   {
+//     rules: {
+//       "prefer-const": "warn",
+//     }
+//   },
+// ];
+
+// export default configs;
