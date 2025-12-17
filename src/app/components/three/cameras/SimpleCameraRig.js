7693aa074a65ad113692b3078cea4f3f5408b44d
pirouzmehmandoost <pirouzmehmandoost@gmail.com>
2025-12-17 02:34:06 -0800

    Add positioning logic on mount and on target tef update

--- src/app/components/three/cameras/SimpleCameraRig.js
+++ src/app/components/three/cameras/SimpleCameraRig.js
@@ -1,47 +1,50 @@
 'use client';
 
 import * as THREE from 'three';
-import { useFrame} from '@react-three/fiber';
-import { useMemo } from 'react'
-// import { easing } from 'maath';
+import { useFrame, useThree } from '@react-three/fiber';
+import { useMemo, useLayoutEffect, useRef } from 'react'
+import { easing } from 'maath';
+import cameraConfigs from '@configs/cameraConfigs';
 
 THREE.Cache.enabled = true;
 
-const SimpleCameraRig = (props) => {
-    const {
-      cameraPosition = [0,0,180],
-      target,
-    } = props;
+const SimpleCameraRig = ({ target, onTargetReady }) => {
+  const { camera } = useThree();
+  const ref = useRef(undefined);
 
-  const cameraTargetPosition = useMemo(() => {
-    const positionVector = new THREE.Vector3(...cameraPosition);
-
-    if (!target) return positionVector;
-
-    target.updateWorldMatrix();
-    const modelBoundingBox = new THREE.Box3().setFromObject(target);
-    positionVector.set(
-      (modelBoundingBox.max.x + modelBoundingBox.min.x)/2,
-      (modelBoundingBox.max.y + modelBoundingBox.min.y)/2,
-      (modelBoundingBox.max.z + modelBoundingBox.min.z)/2,
-    );
+  const cameraTargetPosition = useMemo(()=> {
+    const positionVector = new THREE.Vector3(cameraConfigs);
+    if (target) {
+      target.updateWorldMatrix(true, true);
+      const modelBoundingBox = new THREE.Box3().setFromObject(target);
+      const center = new THREE.Vector3();
+      modelBoundingBox.getCenter(center);
+      positionVector.copy(center);
+    };
     return positionVector;
-  },[target]);
+  }, [target]);
 
-    useFrame(({ clock, camera }, delta) => {
-      const t = clock.elapsedTime;
-      const sin = Math.sin(t/2)*5;
-      const cos = Math.cos(t/2)*50;
-      const x = cameraTargetPosition.x+sin;
-      const y = cameraTargetPosition.y+cos;
-      const z = cameraTargetPosition.z+100+sin;
-
+  useLayoutEffect(() => {
+    if (target) {
+      camera.position.set( cameraTargetPosition.x, cameraTargetPosition.y, cameraTargetPosition.z + 220);
       camera.lookAt(cameraTargetPosition);
-      // easing.damp3(camera.position, [x,y,z], 0.5, delta);
-      const v = new THREE.Vector3(x, y, z)
-      camera.position.lerp(v, 0.06);
+      camera.updateMatrixWorld();
+      if (onTargetReady) onTargetReady(cameraTargetPosition);
+    }
+  }, [target, cameraTargetPosition, camera]);
 
-    });
-  };
+  useFrame(({ clock, camera }, delta) => {
+    camera.lookAt(cameraTargetPosition);
+    camera.updateMatrixWorld();
+    const t = clock.elapsedTime;
+    const xOffset = 50 * Math.sin(t);
+    const yOffset = 50 * Math.cos(t);
+    const zOffset = cameraConfigs.POSITION[2] + yOffset;
+    const v = new THREE.Vector3(cameraTargetPosition.x + xOffset, cameraTargetPosition.y + yOffset,  cameraTargetPosition.z + zOffset)
+    easing.damp3(camera.position, v, 1, 0.06)
+  });
 
-  export default SimpleCameraRig;
+  return <perspectiveCamera ref={ref} />;
+};
+
+export default SimpleCameraRig;
