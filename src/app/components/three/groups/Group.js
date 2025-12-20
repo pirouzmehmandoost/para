ea2abc29c646386854261b25d3b3c2ac9f203ca4
pirouzmehmandoost <pirouzmehmandoost@gmail.com>
2025-12-19 22:16:48 -0800

    update

--- src/app/components/three/groups/Group.js
+++ src/app/components/three/groups/Group.js
@@ -1,6 +1,6 @@
 'use client';
 
-import { useMemo } from 'react';
+import React, { useMemo } from 'react';
 import { useThree } from '@react-three/fiber';
 import { calculateXZPositions, scaleMeshAtBreakpoint } from '@utils/mesh/meshUtils';
 import Model from '../models/Model';
@@ -8,40 +8,43 @@ import Model from '../models/Model';
 const Group = (props) => {
   const { size } = useThree();
   const {
-    autoRotate, 
-    autoRotateSpeed,
-    materials,
-    modelUrls,
+    autoRotate = true, 
+    autoRotateSpeed = 0.5,
     groundMeshRef,
     groupRef,
-    onMeshReady,
-    position: groupPosition,
-    scale,
-    ...rest
+    materials = {},
+    modelUrls = [],
+    onMeshReady = [],
+    position,
+    scale = 1,
+    onClick = () => {},
   } = props;
 
-  const positions = calculateXZPositions(size.width, modelUrls.length, groupPosition);
-  const modelScale = useMemo(()=> modelUrls.length === 1 ? scale * 0.4 : (scaleMeshAtBreakpoint(size.width)/modelUrls.length), [modelUrls.length, size.width]);
+  const modelScale = modelUrls.length === 1 
+    ? scale * 0.4 
+    : (scaleMeshAtBreakpoint(size.width)/modelUrls.length);
+
+  const positions = useMemo(() => 
+    calculateXZPositions(size.width, modelUrls.length, position)
+  , [modelUrls.length, position, size.width]);
 
   return (
-    <group ref={groupRef} {...rest}>
-      {
-        modelUrls.map((modelUrl, index) => (
-          <Model
-            autoRotate={autoRotate}
-            autoRotateSpeed={autoRotateSpeed}
-            onMeshReady={onMeshReady[index]}
-            key={modelUrl.name}
-            groundMeshRef={groundMeshRef}
-            materialId={ modelUrls.length === 1 ? materials.defaultMaterial : Object.values(materials.colorWays)[index] }
-            modelUrl={modelUrl}
-            position={positions[index]}
-            scale={modelScale}
-          />
-        ))
-      }
+    <group ref={groupRef} onClick={onClick}>
+      {modelUrls.map((modelUrl, index) => (
+        <Model
+          key={`${modelUrl?.name ?? modelUrl?.url ?? 'model'}_${index}`}
+          autoRotate={autoRotate}
+          autoRotateSpeed={autoRotateSpeed}
+          groundMeshRef={groundMeshRef}
+          materialId={modelUrls.length === 1 ? materials.defaultMaterial : Object.values(materials.colorWays)[index] }
+          modelUrl={modelUrl}
+          onMeshReady={onMeshReady[index]}
+          position={positions[index] ?? position}
+          scale={modelScale}
+        />
+      ))}
     </group>
   );
 };
 
-export default Group;
+export default React.memo(Group);
