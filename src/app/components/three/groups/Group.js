0a10401f9f8f01d9dadb7c7ef19bf638e46830fc
pirouzmehmandoost <pirouzmehmandoost@gmail.com>
2026-01-03 00:09:38 -0800

    Gut, prepare for evenual deletion

--- src/app/components/three/groups/Group.js
+++ src/app/components/three/groups/Group.js
@@ -1,49 +1,52 @@
 'use client';
 
-import React, { useMemo } from 'react';
-import { useThree } from '@react-three/fiber';
-import { calculateXZPositions, scaleMeshAtBreakpoint } from '@utils/mesh/meshUtils';
+// import React, { useMemo } from 'react';
+import React from 'react';
+// import { useThree } from '@react-three/fiber';
+// import { calculateXZPositions, scaleMeshAtBreakpoint } from '@utils/mesh/meshUtils';
 import Model from '../models/Model';
 
 const Group = (props) => {
-  const { size } = useThree();
+  // const { size } = useThree();
   const {
-    autoRotate = true, 
+    autoRotate = true,
     autoRotateSpeed = 0.5,
-    groundMeshRef,
-    groupRef,
-    materials = {},
-    modelUrls = [],
-    onMeshReady = [],
-    position,
+    groundMeshRef = null,
+    materials: { defaultMaterial = '' } = {},
+    fileData = {},
+    onMeshReady = () => { },
+    position = null,
     scale = 1,
-    onClick = () => {},
+    onClick = () => { },
   } = props;
 
-  const modelScale = modelUrls.length === 1 
-    ? scale * 0.4 
-    : (scaleMeshAtBreakpoint(size.width)/modelUrls.length);
+  // const modelScale = scale * 0.5 * scaleMeshAtBreakpoint(size.width);
 
-  const positions = useMemo(() => 
-    calculateXZPositions(size.width, modelUrls.length, position)
-  , [modelUrls.length, position, size.width]);
+  // const newPosition = useMemo(() => 
+  //   calculateXZPositions(size.width, 1, position)
+  // , [position, size.width]);
+
+  //   const modelScale = modelUrls.length === 1 
+  //   ? scale * 0.4 
+  //   : (scaleMeshAtBreakpoint(size.width)/modelUrls.length);
+
+  // const positions = useMemo(() => 
+  //   calculateXZPositions(size.width, modelUrls.length, position)
+  // , [modelUrls.length, position, size.width]);
 
   return (
-    <group ref={groupRef}>
-      {modelUrls.map((modelUrl, index) => (
-        <Model
-          key={`${modelUrl?.name ?? modelUrl?.url ?? 'model'}_${index}`}
-          autoRotate={autoRotate}
-          autoRotateSpeed={autoRotateSpeed}
-          groundMeshRef={groundMeshRef}
-          materialId={materials.defaultMaterial}
-          modelUrl={modelUrl}
-          onClick={onClick}
-          onMeshReady={onMeshReady[index]}
-          position={positions[index] ?? position}
-          scale={modelScale}
-        />
-      ))}
+    <group>
+      <Model
+        autoRotate={autoRotate}
+        autoRotateSpeed={autoRotateSpeed}
+        groundMeshRef={groundMeshRef}
+        materialId={defaultMaterial}
+        fileData={fileData}
+        onClick={onClick}
+        onMeshReady={onMeshReady}
+        position={position}
+        scale={scale}
+      />
     </group>
   );
 };
