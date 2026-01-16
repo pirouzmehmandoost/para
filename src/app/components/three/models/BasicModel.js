16301914e1b36a41d39525023d80cb019fc52438
pirouzmehmandoost <pirouzmehmandoost@gmail.com>
2026-01-15 22:16:19 -0800

    Damp material properties per subscription to Zustand stores

--- src/app/components/three/models/BasicModel.js
+++ src/app/components/three/models/BasicModel.js
@@ -4,23 +4,40 @@ import { useEffect, useRef } from 'react';
 import * as THREE from 'three';
 import { useFrame } from '@react-three/fiber';
 import { useGLTF } from '@react-three/drei';
+import { easing } from 'maath';
+import { envColor } from '@configs/globals';
 import useMaterial from '@stores/materialStore';
 import useSelection from '@stores/selectionStore';
-import { easing } from 'maath';
 
+THREE.ColorManagement.enabled = true;
 THREE.Cache.enabled = true;
+
 useGLTF.preload('/bag_v5_for_web-transformed.glb');
 useGLTF.preload('/yoga_mat_strap_for_web2.glb');
 useGLTF.preload('/bag_v3.5-transformed.glb');
 useGLTF.preload('/bag_9_BAT-transformed.glb');
+
+const MATERIAL_PROPS = {
+  clearcoat: 0,
+  clearcoatRoughness: 0,
+  flatShading: false,
+  ior: 1.5,
+  metalness: 0,
+  reflectivity: 0.3,
+  roughness: 0.8,
+  sheen: 0.04,
+  sheenColor: envColor,
+  sheenRoughness: 0.1,
+  side: THREE.DoubleSide,
+  color: '#2f2f2f',
+}
 
 const BasicModel = (props) => {
   const {
     autoRotate = true,
     autoRotateSpeed = 0.5,
     fileData: { nodeName = '', url = '' } = {},
-    materialId = '',
-    materials: { defaultMaterial = '' } = {},
+    materials: { defaultMaterialID = '' } = {},
     onClick = undefined,
     onMeshReady = undefined,
     position = { x: 0, y: 0, z: 0 },
@@ -30,18 +47,40 @@ const BasicModel = (props) => {
 
   const geometry = useGLTF(url).nodes?.[nodeName]?.geometry || null;
   const meshRef = useRef(undefined);
+
   const animateRotationRef = useRef(new THREE.Euler());
   const animatePositionRef = useRef(new THREE.Vector3(0, 0, 0));
-  const isFocused = useSelection((state) => state.selection.isFocused);
-  const getMaterial = useMaterial((state) => state.getMaterial);
 
-  const matId = materialId?.length ? materialId : defaultMaterial;
-  const material = getMaterial(matId)?.material;
+  // parent event handlers update isFocused and selectedMaterialID
+  const isFocused = useSelection((state) => state.selection.isFocused);
+  const selectedMaterialID = useSelection((state) => state.selection.materialID); 
+
+  const materials = useMaterial(state => state.materials);
+
+  const selectedMaterialRef = useRef(null); // points to an instance of MeshPhysicalMaterial in materialStore. The material is a property of materialStore[id]
+  const defaultMaterialRef = useRef(new THREE.MeshPhysicalMaterial({ ...MATERIAL_PROPS }));
+  const blendedMaterialRef = useRef(new THREE.MeshPhysicalMaterial({ ...MATERIAL_PROPS }));  // useFrame updates material properties
+
+
+  const selectedMatID =
+    selectedMaterialID.length &&
+    isFocused?.length &&
+    isFocused === nodeName
+      ? selectedMaterialID
+      : defaultMaterialID;
+
+  const selectedMat = materials[selectedMatID]?.material;
+  selectedMaterialRef.current = selectedMat
+
+  const defaultMat = materials[defaultMaterialID]?.material;
+  defaultMaterialRef.current.copy(defaultMat);
 
   useEffect(() => {
     if (meshRef.current) {
       meshRef.current.updateWorldMatrix(true, true);
-      if (onMeshReady) onMeshReady(meshRef.current);
+      blendedMaterialRef.current.copy(defaultMaterialRef.current);
+
+      if (typeof onMeshReady === 'function') onMeshReady(meshRef.current);
     }
   }, [onMeshReady]);
 
@@ -56,11 +95,31 @@ const BasicModel = (props) => {
       if (autoRotate) {
         if (isFocused?.length && isFocused === nodeName) {
           animatePositionRef.current.set(meshRef.current.position.x + sine, meshRef.current.position.y + sine, meshRef.current.position.z + sine);
-          animateRotationRef.current.set(0, Math.PI*rotation, 0);
+          animateRotationRef.current.set(0, Math.PI * rotation, 0);
           easing.damp3(meshRef.current.position, animatePositionRef.current, 1, clampedDelta);
           easing.dampE(meshRef.current.rotation, animateRotationRef.current, 1.5, clampedDelta);
+
+          easing.dampC(blendedMaterialRef.current.color, selectedMaterialRef.current.color, 0.3, clampedDelta)
+          easing.damp(blendedMaterialRef.current, "clearcoat", selectedMaterialRef.current?.clearcoat ?? 0, 0.3, clampedDelta);
+          easing.damp(blendedMaterialRef.current, "clearcoatRoughness", selectedMaterialRef.current?.clearcoatRoughness ?? 0, 0.3, clampedDelta);
+          easing.damp(blendedMaterialRef.current, "ior", selectedMaterialRef.current.ior, 0.3, clampedDelta);
+          easing.damp(blendedMaterialRef.current, "metalness", selectedMaterialRef.current?.metalness ?? 0, 0.3, clampedDelta);
+          easing.damp(blendedMaterialRef.current, "reflectivity", selectedMaterialRef.current?.reflectivity ?? 0, 0.3, clampedDelta);
+          easing.damp(blendedMaterialRef.current, "roughness", selectedMaterialRef.current?.roughness ?? 0, 0.3, clampedDelta);
+          easing.damp(blendedMaterialRef.current, "sheen", selectedMaterialRef.current?.sheen ?? 0, 0.3, clampedDelta);
+          easing.damp(blendedMaterialRef.current, "sheenRoughness", selectedMaterialRef.current?.sheenRoughness ?? 0, 0.3, clampedDelta);
         }
         else {
+          easing.dampC(blendedMaterialRef.current.color, defaultMaterialRef.current.color, 0.3, clampedDelta)
+          easing.damp(blendedMaterialRef.current, "clearcoat", defaultMaterialRef.current?.clearcoat ?? 0, 0.3, clampedDelta);
+          easing.damp(blendedMaterialRef.current, "clearcoatRoughness", defaultMaterialRef.current?.clearcoatRoughness ?? 0, 0.3, clampedDelta);
+          easing.damp(blendedMaterialRef.current, "ior", defaultMaterialRef.current.ior ?? 1.5, 0.3, clampedDelta);
+          easing.damp(blendedMaterialRef.current, "metalness", defaultMaterialRef.current?.metalness ?? 0, 0.3, clampedDelta);
+          easing.damp(blendedMaterialRef.current, "reflectivity", defaultMaterialRef.current?.reflectivity ?? 0, 0.3, clampedDelta);
+          easing.damp(blendedMaterialRef.current, "roughness", defaultMaterialRef.current?.roughness ?? 0, 0.3, clampedDelta);
+          easing.damp(blendedMaterialRef.current, "sheen", defaultMaterialRef.current?.sheen ?? 0.04, 0.3, clampedDelta);
+          easing.damp(blendedMaterialRef.current, "sheenRoughness", defaultMaterialRef.current?.sheenRoughness ?? 0.1, 0.3, clampedDelta);
+
           animatePositionRef.current.set(...meshRef.current.position);
           animateRotationRef.current.set(0, meshRef.current.rotation.y, 0);
           meshRef.current.rotation.y += delta * autoRotateSpeed;
@@ -76,14 +135,13 @@ const BasicModel = (props) => {
           ref={meshRef}
           castShadow={true}
           geometry={geometry}
-          material={material}
+          material={blendedMaterialRef.current}
           name={nodeName}
           onClick={onClick}
           position={position}
           receiveShadow={true}
           rotation={[0, Math.PI * rotation, 0]}
           scale={scale}
-
         />
       )}
     </>
