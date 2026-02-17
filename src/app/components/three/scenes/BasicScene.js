96a43287fdb0e64b756b4269a27c5ac334b03b0e
pirouzmehmandoost <pirouzmehmandoost@gmail.com>
2026-02-16 18:31:33 -0800

    Change y position

--- src/app/components/three/scenes/BasicScene.js
+++ src/app/components/three/scenes/BasicScene.js
@@ -66,7 +66,7 @@ const BasicScene = () => {
   }, [signature, textures, setMaterialTextures, texturesToLoad]);
 
   const meshPositions = useMemo(() => {
-    const fixedYPositions = [-10, 40, -120];
+    const fixedYPositions = [-10, 40, -110];
 
     const ellipseRadius = scaleMeshAtBreakpoint(size.width) * 130;
     const positions = [];
