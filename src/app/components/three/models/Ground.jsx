8261e135e1496f9bb66e2a24bfbe70193420499e
pirouzmehmandoost <pirouzmehmandoost@gmail.com>
2026-03-08 01:35:06 -0800

    Refactor r3f components

--- src/app/components/three/models/Ground.jsx
+++ src/app/components/three/models/Ground.jsx
@@ -20,30 +20,20 @@ const Ground = (props) => {
     onGroundReady = undefined,
     position = [],
     rotation = [],
-    scale,
+    scale = [],
   } = props;
 
-  let groundScale = [];
   const groundRef = useRef(undefined);
-  const hasPositioned = useRef(false);
+  const hasPositionedRef = useRef(false);
   const geometry = useGLTF(URL).nodes?.Plane?.geometry ?? null;
   const materials = useMaterial.getState().materials;
   const material = materials[NODE_NAME].material;
   const groundPosition = position?.length ? position : POSITION;
   const groundRotation = rotation?.length ? rotation : ROTATION;
-
-  if (Array.isArray(scale) && scale?.length) {
-    groundScale = [...scale];
-  }
-  else if (typeof scale === 'number') {
-    groundScale = [SCALE[0] * scale, SCALE[1] * scale, SCALE[2] * scale];
-  }
-  else {
-    groundScale = [...SCALE];
-  }
+  const groundScale = scale?.length === 3 ? scale : SCALE;
 
   useEffect(() => {
-    if (hasPositioned.current) return;
+    if (hasPositionedRef.current) return;
 
     if (groundRef.current) {
       groundRef.current?.updateMatrixWorld(true, true);
@@ -51,7 +41,7 @@ const Ground = (props) => {
       if (typeof onGroundReady === 'function') {
         onGroundReady(groundRef.current);
       }
-      hasPositioned.current = true;
+      hasPositionedRef.current = true;
     }
   }, [onGroundReady]);
 
