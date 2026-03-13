102f2cb75025889e58575771e1e075c7d8c78223
pirouzmehmandoost <pirouzmehmandoost@gmail.com>
2026-03-12 17:51:40 -0700

    Remove ellipse-based mesh prositioning logic

--- src/app/components/three/scenes/BasicScene.js
+++ src/app/components/three/scenes/BasicScene.js
@@ -4,11 +4,10 @@ import { startTransition, useCallback, useEffect, useMemo, useRef, useState } fr
 import * as THREE from 'three';
 import { useThree } from '@react-three/fiber';
 import { Bvh, } from '@react-three/drei'
-import { EffectComposer, N8AO } from '@react-three/postprocessing';
+import { EffectComposer, N8AO, Vignette } from '@react-three/postprocessing';
 // import { BlurPass, BlendFunction, Resizer, KernelSize } from 'postprocessing'
 import { portfolio } from '@configs/globals';
 import cameraConfigs from '@configs/cameraConfigs';
-import { scaleMeshAtBreakpoint } from '@utils/scaleUtils';
 import useSelection from '@stores/selectionStore';
 import MaterialTextureInitializer from '../textures/MaterialTextureInitializer';
 import AnimatedRig from '../cameras/AnimatedRig';
@@ -18,52 +17,33 @@ import Ground from '../models/Ground';
 THREE.Cache.enabled = true;
 THREE.ColorManagement.enabled = true;
 
-const { projects } = portfolio;
-
 const BasicScene = () => {
+  const { projects } = portfolio;
   const { SWIPE_DELAY_MS } = cameraConfigs;
-  const size = useThree((state) => state.size);
+  const totalMeshes = projects.length;
+
   const set = useThree((state) => state.set);
   const get = useThree((state) => state.get);
+
   const setSelectionStore = useSelection((state) => state.setSelection);
   const isFocused = useSelection((state) => state.selection.isFocused);
   const setIsFocused = useSelection((state) => state.setIsFocused);
   const setMaterialID = useSelection((state) => state.setMaterialID);
   const resetSelectionStore = useSelection((state) => state.reset);
-  const lastSwipeTimeRef = useRef(0);
-  const readyCount = useRef(0);
+
   const [meshesReady, setMeshesReady] = useState(false);
+
+  const lastSwipeTimeRef = useRef(0);
+  const meshesReadyCount = useRef(0);
   const meshRefs = useRef(new Array(projects.length).fill(null));
   const meshReadyFlags = useRef(new Array(projects.length).fill(false));
-  const totalMeshes = projects.length;
+  const meshPositions = useRef([
+    new THREE.Vector3(-100, -15, -40),
+    new THREE.Vector3(100, 25, -40),
+    new THREE.Vector3(0, -105, 40)
+  ]);
+
   const cameraTargets = useMemo(() => meshesReady ? meshRefs.current : [], [meshesReady]);
-  // const planeRef = useRef(null);
-  // const lightRef = useRef(null);
-  // const blurPassRef = useRef(new BlurPass());
-
-  const groundScale = Math.min(0.6, scaleMeshAtBreakpoint(size.width) * 0.6);
-
-  const meshPositions = useMemo(() => {
-    const finalPositions = [];
-    const fixedYPositions = [-15, 25, -110];
-    const ellipseRadius = scaleMeshAtBreakpoint(size.width) * 95;
-    const vertex = new THREE.Vector3();
-    const ellipseCurve = new THREE.EllipseCurve(0, 0, ellipseRadius, ellipseRadius, 0, 2 * Math.PI, false, 0.5 * Math.PI);
-    ellipseCurve.closed = true;
-
-    const curvePoints = ellipseCurve.getPoints(projects.length);
-    const ellipseCurvePoints = curvePoints.slice(1);
-    const positionAttribute = new THREE.BufferGeometry()
-      .setFromPoints(ellipseCurvePoints)
-      .getAttribute('position');
-
-    for (let i = 0; i < positionAttribute.count; i++) {
-      const xzPosition = vertex.fromBufferAttribute(positionAttribute, i);
-      finalPositions.push(new THREE.Vector3(xzPosition.x, fixedYPositions[i], xzPosition.y));
-    }
-
-    return finalPositions;
-  }, [size.width]);
 
   const meshReadyHandlers = useMemo(() =>
     projects.map((_, i) => {
@@ -72,12 +52,10 @@ const BasicScene = () => {
 
         meshRefs.current[i] = mesh;
         meshReadyFlags.current[i] = true;
-        readyCount.current += 1;
-
-        if (readyCount.current === totalMeshes) setMeshesReady(true);
+        meshesReadyCount.current += 1;
+        if (meshesReadyCount.current === totalMeshes) setMeshesReady(true);
       }
-    }
-    ), [totalMeshes]);
+    }), [totalMeshes]);
 
   const handlePointerMissed = useCallback((e) => {
     if (Date.now() - lastSwipeTimeRef.current < SWIPE_DELAY_MS) return;
@@ -125,7 +103,7 @@ const BasicScene = () => {
         castShadow={true}
         color={'#fff6e8'}
         intensity={2}
-        position={[0, 120, 75]}
+        position={[0, 120, 80]}
         shadow-bias={-0.004}
         shadow-camera-fov={50}
         shadow-camera-near={1}
@@ -134,23 +112,25 @@ const BasicScene = () => {
         shadow-camera-bottom={-250}
         shadow-camera-left={-250}
         shadow-camera-right={250}
-        shadow-mapSize={2048}
+        shadow-mapSize={4096}
       />
-      <EffectComposer autoClear={false} disableNormalPass multisampling={0}>
-        <N8AO aoRadius={50} distanceFalloff={0.3} intensity={0.5} screenSpaceRadius halfRes />
-        {/* <Vignette eskil={false} offset={0.01} darkness={0.7} /> */}
-        {/* <SelectiveBloom
-          blurPass={blurPassRef.current} 
-          height={Resizer.AUTO_SIZE} 
-          intensity={0.1}
-          kernelSize={KernelSize.LARGE} 
-          lights={lightRef}
-          luminanceSmoothing={0.5} // smoothness of the luminance threshold. Range is [0, 1]
-          luminanceThreshold={0.025} // luminance threshold. Raise this value to mask out darker elements in the scene.
-          selection={planeRef} 
-          selectionLayer={4}
-          width={Resizer.AUTO_SIZE}
-        /> */}
+      <EffectComposer
+        autoClear={false}
+        disableNormalPass
+        multisampling={0}
+      >
+        <N8AO
+          aoRadius={50}
+          distanceFalloff={0.3}
+          intensity={0.5}
+          screenSpaceRadius
+          halfRes
+        />
+        <Vignette
+          eskil={false}
+          offset={0.01}
+          darkness={0.75}
+        />
       </EffectComposer>
       <Bvh firstHitOnly>
         {projects.map(({ sceneData, sceneData: { fileData: { nodeName } = {} } = {} }, index) => {
@@ -164,33 +144,20 @@ const BasicScene = () => {
               name={nodeName}
               onClick={handleClick}
               onMeshReady={meshReadyHandlers[index]}
-              position={meshPositions[index]}
+              position={meshPositions.current[index]}
               rotation={sceneData.rotation}
               scale={sceneData.scale}
             />
           );
         })}
       </Bvh>
-      {/* <mesh
-        ref={planeRef}
-        position={[78, 25, -120]}
-        scale={[25, 25, 25]}
-      >
-        <planeGeometry />
-        <meshPhysicalMaterial
-          color='white'
-          emissive='white'
-          emissiveIntensity={5}
-          side={THREE.DoubleSide}
-        />
-      </mesh> */}
       <Ground
         position={[0, -90, -15]}
-        rotation={[Math.PI / 5, Math.PI, 0]}
-        scale={[groundScale * 1.3, groundScale, groundScale * 1.3]}
+        rotation={[Math.PI / 4.5, Math.PI, 0]}
+        scale={[0.7, 0.7, 0.7]}
       />
       <AnimatedRig
-        fallbackPositions={meshPositions}
+        fallbackPositions={meshPositions.current}
         focusTarget={isFocused}
         onSwipe={onSwipe}
         targets={cameraTargets}
