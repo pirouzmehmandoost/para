4bf5d157d663be579bf5c656a1f6cd0dc797d0d2
pirouzmehmandoost <pirouzmehmandoost@gmail.com>
2026-03-31 14:46:25 -0700

    Consolidate scene-related configs into sceneConfigs, update HomePageScene

--- src/app/components/three/scenes/HomePageScene.js
+++ src/app/components/three/scenes/HomePageScene.js
@@ -6,45 +6,55 @@ import { useThree } from '@react-three/fiber';
 import { Cloud, Clouds } from '@react-three/drei'
 import { EffectComposer, N8AO, SelectiveBloom } from '@react-three/postprocessing';
 import { BlurPass, BlendFunction, Resizer, KernelSize } from 'postprocessing'
-import { portfolio } from '@configs/globals';
+import sceneConfigs from '@configs/sceneConfigs';
 import { scaleMeshAtBreakpoint } from '@utils/scaleUtils';
 import MaterialTextureInitializer from '../textures/MaterialTextureInitializer';
 import SimpleCameraRig from '../cameras/SimpleCameraRig';
-import BasicModel from '../models/BasicModel';
+import Model from '../models/Model';
 import Ground from '../models/Ground';
 
 THREE.Cache.enabled = true;
 THREE.ColorManagement.enabled = true;
 
-const { projects } = portfolio;
-const { sceneData, sceneData: { fileData: { nodeName, } = {}, } = {}, } = projects[1];
+const { projects } = sceneConfigs;
+const {
+  sceneData = {},
+  sceneData: {
+    fileData: {
+      nodeName = '',
+    } = {},
+  } = {},
+} = projects[`Gerd`];
 
 const HomePageScene = () => {
   const size = useThree((state) => state.size);
+  
+  const totalMeshes = 1;
+  const groundMeshScale = Math.min(0.5, scaleMeshAtBreakpoint(size.width) * 0.5);
+  const cloudScale = Math.min(1.5, scaleMeshAtBreakpoint(size.width) * 1.5);
 
-  const readyCount = useRef(0);
+  const meshReadyCountRef = useRef(0);
   const blurPassRef = useRef(new BlurPass());
   const lightRef = useRef(undefined);
   const planeRef = useRef(undefined);
   const meshRef = useRef(undefined);
-  const meshPositionRef = useRef(new THREE.Vector3(0,0,0));
+  const meshPositionRef = useRef(new THREE.Vector3(0, 0, 0));
   const cameraFallbackPositionRef = useRef(new THREE.Vector3(0, 50, 50));
   const meshReadyFlag = useRef(false);
 
   const [meshReady, setMeshReady] = useState(false);
+
   const cameraTarget = useMemo(() => meshReady ? meshRef.current : null, [meshReady]);
 
-  const totalMeshes = 1;
-  const meshScale = Math.min(0.5, scaleMeshAtBreakpoint(size.width) * 0.5);
-  const cloudScale = Math.min(1.5, scaleMeshAtBreakpoint(size.width) * 1.5);
-
-  const meshReadyHandlers = useMemo(() => {
+  const meshReadyHandler = useMemo(() => {
     if (meshReadyFlag.current) return;
+
     return (mesh) => {
-        meshRef.current = mesh;
-        meshReadyFlag.current = true;
-        readyCount.current += 1;
-      if (readyCount.current === totalMeshes) setMeshReady(true);
+      meshRef.current = mesh;
+      meshReadyFlag.current = true;
+      meshReadyCountRef.current += 1;
+
+      if (meshReadyCountRef.current === totalMeshes) setMeshReady(true);
     }
   }, []);
 
@@ -67,14 +77,6 @@ const HomePageScene = () => {
         shadow-camera-right={500}
         shadow-mapSize={2048}
       />
-      {/*
-      <AnimatedLight
-        intensity={5}
-        type='directionalLight'
-        color='#FFF6E8'
-        castShadow={false}
-      />
-      */}
       <Clouds material={THREE.MeshPhysicalMaterial} limit={6}>
         <Cloud
           color={'black'}
@@ -103,31 +105,40 @@ const HomePageScene = () => {
           fade={5}
         />
       </Clouds>
-      <EffectComposer autoClear={false} disableNormalPass multisampling={0}>
-        <N8AO aoRadius={270} distanceFalloff={0.5} intensity={10} />
+      <EffectComposer
+        autoClear={false}
+        disableNormalPass
+        multisampling={0}
+      >
+        <N8AO
+          aoRadius={270}
+          distanceFalloff={0.5}
+          intensity={10}
+        />
         <SelectiveBloom
-          blurPass={blurPassRef.current} 
-          height={Resizer.AUTO_SIZE} 
+          blurPass={blurPassRef.current}
+          height={Resizer.AUTO_SIZE}
           intensity={0}
-          kernelSize={KernelSize.LARGE} 
+          kernelSize={KernelSize.LARGE}
           lights={lightRef}
-          luminanceSmoothing={0.05} // smoothness of the luminance threshold. Range is [0, 1]
-          luminanceThreshold={0.05} // luminance threshold. Raise this value to mask out darker elements in the scene.
-          selection={planeRef} 
+          luminanceSmoothing={0.05} // Range is [0, 1]
+          luminanceThreshold={0.05} // Raise to mask out darker elements in the scene.
+          selection={planeRef}
           selectionLayer={2}
           width={Resizer.AUTO_SIZE}
         />
       </EffectComposer>
-      <BasicModel
-        key={nodeName}
-        autoRotate={false}
-        autoRotateSpeed={sceneData.autoRotateSpeed}
+      <Model
+        animateMaterial={false}
+        animatePosition
+        animateRotation={false}
         fileData={sceneData.fileData}
         materials={sceneData.materials}
         name={nodeName}
-        onMeshReady={meshReadyHandlers}
+        onMeshReady={meshReadyHandler}
         position={meshPositionRef.current}
         rotation={sceneData.rotation}
+        // rotationSpeed={sceneData.rotationSpeed}
         scale={sceneData.scale}
       />
       <mesh
@@ -148,9 +159,9 @@ const HomePageScene = () => {
         focusTarget={cameraTarget}
         fallbackPosition={cameraFallbackPositionRef.current}
       />
-      <Ground 
-        rotation={[Math.PI/3.5, 0, 0]}
-        scale={[meshScale * 1.3, meshScale * 1.3, meshScale * 1.3]} 
+      <Ground
+        rotation={[Math.PI / 3.5, 0, 0]}
+        scale={[groundMeshScale * 1.3, groundMeshScale * 1.3, groundMeshScale * 1.3]}
         position={[0, -60, 0]}
       />
 
