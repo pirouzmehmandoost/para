725c29d347c7479e8001527f3e74822214048399
pirouzmehmandoost <pirouzmehmandoost@gmail.com>
2026-03-31 16:08:53 -0700

    format file, update variable names

--- src/app/components/three/cameras/SimpleCameraRig.js
+++ src/app/components/three/cameras/SimpleCameraRig.js
@@ -4,15 +4,26 @@ import * as THREE from 'three';
 import { useFrame, useThree } from '@react-three/fiber';
 import { useLayoutEffect, useRef } from 'react'
 import { easing } from 'maath';
-import cameraConfigs from '@configs/cameraConfigs';
+import sceneConfigs from '@configs/sceneConfigs';
+import { capitalize } from '@mui/material';
 
 THREE.Cache.enabled = true;
+const {
+  cameraConfigs: {
+    POSITION: [configX, configY, configZ],
+  } = {},
+} = sceneConfigs;
 
-const SimpleCameraRig = ({ focusTarget, fallbackPosition, cameraShake, }) => {
+const SimpleCameraRig = ({
+  focusTarget,
+  fallbackPosition,
+  cameraShake = false
+}) => {
   const sceneCamera = useThree((s) => s.camera);
   const lookAtPositionRef = useRef(new THREE.Vector3(sceneCamera.position.x, sceneCamera.position.y, sceneCamera.position.z));
   const cameraPositionRef = useRef(new THREE.Vector3(sceneCamera.position.x, sceneCamera.position.y, sceneCamera.position.z));
   const blendedPositionRef = useRef(new THREE.Vector3(sceneCamera.position.x, sceneCamera.position.y, sceneCamera.position.z));
+  // const defaultFallBackPosition = useRef(new THREE.Vector3(configX, configY, configZ))
   const _scratchBoxRef = useRef(new THREE.Box3());
   const _scratchCenterRef = useRef(new THREE.Vector3());
 
@@ -20,20 +31,20 @@ const SimpleCameraRig = ({ focusTarget, fallbackPosition, cameraShake, }) => {
     if (focusTarget && focusTarget?.isObject3D) {
       focusTarget.updateWorldMatrix(true, true);
       _scratchBoxRef.current
-      .setFromObject(focusTarget)
-      .getCenter(_scratchCenterRef.current);
+        .setFromObject(focusTarget)
+        .getCenter(_scratchCenterRef.current);
 
       lookAtPositionRef.current.set(
         _scratchCenterRef.current.x,
         _scratchCenterRef.current.y,
-        _scratchCenterRef.current.z + cameraConfigs.POSITION[2] + 10
+        _scratchCenterRef.current.z + configZ + 10
       );
     }
-    else {
+    else if (fallbackPosition.isObject3D){
       lookAtPositionRef.current.set(
         fallbackPosition.x,
         fallbackPosition.y,
-        fallbackPosition.z + cameraConfigs.POSITION[2] + 10
+        fallbackPosition.z + configZ + 10
       );
     }
     blendedPositionRef.current.copy(lookAtPositionRef.current);
@@ -45,10 +56,14 @@ const SimpleCameraRig = ({ focusTarget, fallbackPosition, cameraShake, }) => {
 
     if (cameraShake) {
       const elapsedTime = clock.elapsedTime;
+      const xOffset = Math.sin(elapsedTime) * 2.5;
+      const yOffset = Math.cos(elapsedTime) * 5;
+      const zOffset = Math.cos(elapsedTime) * -2;
+
       blendedPositionRef.current.set(
-        lookAtPositionRef.current.x + 2.5 * Math.sin(elapsedTime),
-        lookAtPositionRef.current.y + 5 * Math.cos(elapsedTime),
-        lookAtPositionRef.current.z + (-2 * Math.cos(elapsedTime))
+        lookAtPositionRef.current.x + xOffset,
+        lookAtPositionRef.current.y + yOffset,
+        lookAtPositionRef.current.z + zOffset
       );
     }
     cameraPositionRef.current.copy(camera.position)
