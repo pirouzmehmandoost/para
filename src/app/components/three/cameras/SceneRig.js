501806c72b21db570947851b0921f525287c183a
pirouzmehmandoost <pirouzmehmandoost@gmail.com>
2026-05-09 01:59:30 -0700

    use getAABBCenterFast()  in SceneRig, implement event-based pattern to target asynchronously mounted scene children

--- src/app/components/three/cameras/SceneRig.js
+++ src/app/components/three/cameras/SceneRig.js
@@ -6,6 +6,8 @@ import { useFrame, useThree } from '@react-three/fiber';
 import { easing } from 'maath';
 import cameraConfigs from '@configs/cameraConfigs';
 import useSelection from '@stores/selectionStore';
+// import { EPSILON_10e4 } from '@/lib/utils/animationUtils';
+import { getAABBCenterFast } from '@/lib/utils/positionUtils';
 
 const SceneRig = ({
   onSwipe = undefined, // optional callback
@@ -34,36 +36,120 @@ const SceneRig = ({
   const lastSwitchTimeRef = useRef(0);
   const manualOverrideTimeRef = useRef(-Infinity); // active while elapsedTime < this value)
 
-  // const currentCameraPositionRef = useRef(new THREE.Vector3());
   const nextCameraPositionRef = useRef(new THREE.Vector3());
   const cameraStopPositionsRef = useRef([new THREE.Vector3(0, 0, 0)]);
   const defaultFallbackPositionRef = useRef(new THREE.Vector3(OFFSET_CAMERA_POSITION[0], OFFSET_CAMERA_POSITION[1], OFFSET_CAMERA_POSITION[2]));
 
   const targetIndexRef = useRef(0);
   const prevTargetIndexRef = useRef(-1);
-  const meshesInSceneRef = useRef({});
+  const targetsInSceneRef = useRef({});
+  const targetsNotInSceneRef = useRef({});
+  const nameToUUIDRef = useRef({});
 
   useLayoutEffect(() => {
     cam.lookAt(INITIAL_CAMERA_POSITION[0], INITIAL_CAMERA_POSITION[1], -1 * INITIAL_CAMERA_POSITION[2]);
   }, [cam]);
 
   useEffect(() => {
-    const meshesInScene = {};
+    const targetsInScene = {};
+    const nameToUUID = {};
+    targetsInSceneRef.current = {};
+    targetsNotInSceneRef.current = {};
 
-    scene.traverse((object) => { if (object.isMesh) meshesInScene[`${object.name}`] = object });
+    scene.traverse((object) => { if (object.isObject3D) targetsInScene[`${object.uuid}`] = object });
 
     for (let i = 0; i < targets.length; i++) {
-      if (targets[i]?.name?.length) {
-        const targetName = targets[i].name;
-        const foundTargetInScene = meshesInScene[targetName] || null;
-
+      if (targets[i]?.uuid?.length) {
+        const targetUUID = targets[i].uuid;
+        const foundTargetInScene = targetsInScene[targetUUID] || null;
         if (foundTargetInScene) {
-          meshesInSceneRef.current[targetName] = { target: foundTargetInScene, index: i, targetName };
+          targetsInSceneRef.current[targetUUID] = { target: foundTargetInScene, index: i, targetUUID };
+          if (foundTargetInScene.name?.length) nameToUUID[foundTargetInScene.name] = targetUUID;
+        }
+        else {
+          targetsNotInSceneRef.current[targetUUID] = { target: targets[i], index: i, targetUUID };
         }
       }
     }
-  }, [targets, scene, scene.children]);
 
+    nameToUUIDRef.current = nameToUUID;
+  }, [targets, scene]);
+
+  useEffect(() => {
+    const addedEventHandlers = [];
+    // const removedEventHandlers = [];
+
+    // for (const [uuid, entry] of Object.entries(targetsInSceneRef.current)) {
+    //   const removedEventHandler = () => {
+    //     // index of cameraStopPositionsRef to update (element is currently a fallback Vector3).
+    //     const staleTargetIndex = entry.index;
+    //     // get parent uuid for validation (Object3D _addedEvent signals parentage and sets Object3D.parent to non-null).
+    //     const parentUUID = entry.target.parent?.uuid;
+    //     // Validate parent in scene graph (parentage does not imply parent in scene, though the parent can be the scene). 
+    //     const isParentInScene = parentUUID ? scene?.getObjectByProperty('uuid', parentUUID)?.isObject3D : null;
+    //     if (isParentInScene) return;
+    //     // Update targetsNotInSceneRef.
+    //     targetsNotInSceneRef.current[uuid] = entry;
+    //     const staleStopPosition = cameraStopPositionsRef.current[staleTargetIndex]?.isVector3 ?  cameraStopPositionsRef.current[staleTargetIndex] : null
+    //     // Update cameraStopPositionsRef.current
+    //     if (staleStopPosition) {
+    //       cameraStopPositionsRef.current.splice(staleTargetIndex, 1);
+    //     }
+    //     // Update name to uuid map.
+    //     if (entry.target?.name?.length) delete nameToUUIDRef.current[entry.target?.name];
+    //     // Update targetsInSceneRef.
+    //     delete targetsInSceneRef.current[uuid];
+    //     // Remove self after firing
+    //     entry.target.removeEventListener('removed', removedEventHandler);
+    //   };
+    //   entry.target.addEventListener('removed', removedEventHandler);
+    //   removedEventHandlers.push({ target: entry.target, removedEventHandler });
+    // }
+
+    for (const [uuid, entry] of Object.entries(targetsNotInSceneRef.current)) {
+      const addedEventHandler = () => {
+        // index of cameraStopPositionsRef to update (element is currently a fallback Vector3).
+        const pendingTargetIndex = entry.index;
+        // get parent uuid for validation (Object3D _addedEvent signals parentage and sets Object3D.parent to non-null).
+        const parentUUID = entry.target.parent?.uuid;
+        // Validate parent in scene graph (parentage does not imply parent in scene, though the parent can be the scene). 
+        const isParentInScene = parentUUID ? scene?.getObjectByProperty('uuid', parentUUID)?.isObject3D : null;
+        if (!isParentInScene) return;
+        // Update targetsInSceneRef.
+        targetsInSceneRef.current[uuid] = entry;
+        // Get AABB center position.
+        const newStopPosition = getAABBCenterFast(entry.target, _scratchCenterRef.current);
+        // Update cameraStopPositionsRef.current[index].
+        if (newStopPosition) {
+          if (cameraStopPositionsRef.current[pendingTargetIndex]?.isVector3) cameraStopPositionsRef.current[pendingTargetIndex].copy(newStopPosition);
+          else {
+            while (cameraStopPositionsRef.current.length <= pendingTargetIndex) {
+              cameraStopPositionsRef.current.push(new THREE.Vector3().copy(defaultFallbackPositionRef.current));
+            }
+            cameraStopPositionsRef.current[pendingTargetIndex].copy(newStopPosition);
+          }
+        }
+        // Update name to uuid map.
+        if (entry.target?.name?.length) nameToUUIDRef.current[entry.target?.name] = uuid;
+        // Update targetsNotInSceneRef.
+        delete targetsNotInSceneRef.current[uuid];
+        // Remove self after firing
+        entry.target.removeEventListener('added', addedEventHandler);
+      };
+
+      entry.target.addEventListener('added', addedEventHandler);
+      addedEventHandlers.push({ target: entry.target, addedEventHandler });
+    }
+    return () => {
+      for (const { target, addedEventHandler } of addedEventHandlers) {
+        target.removeEventListener('added', addedEventHandler);
+      }
+      // for (const { target, removedEventHandler } of removedEventHandlers) {
+      //   target.removeEventListener('removed', removedEventHandler);
+      // }
+
+    };
+  }, [targets, scene]);
 
   useEffect(() => {
     if (!domElement) return;
@@ -128,7 +214,7 @@ const SceneRig = ({
   }, [domElement, clock, onSwipe, SWIPE_DELTA_TIME_MS, SWIPE_DELTA_PX, MANUAL_OVERRIDE_SECONDS]);
 
   useEffect(() => {
-    const length = Math.max(Object.entries(meshesInSceneRef.current)?.length ?? 0, fallbackPositions?.length ?? 0);
+    const length = Math.max(Object.entries(targetsInSceneRef.current)?.length ?? 0, fallbackPositions?.length ?? 0);
 
     for (let i = 0; i < length; i++) {
       if (!cameraStopPositionsRef.current[i]?.isVector3) cameraStopPositionsRef.current[i] = new THREE.Vector3();
@@ -138,12 +224,15 @@ const SceneRig = ({
         continue;
       }
 
-      const key = targets[i]?.name;
-      const exists = meshesInSceneRef.current[key].target?.name;
-      if (exists?.length && typeof targets[i]['updateWorldMatrix'] === 'function') meshesInSceneRef.current[key].target.updateWorldMatrix(true, false);
+      const key = targets[i]?.uuid;
+      const exists = targetsInSceneRef.current[key]?.target?.uuid ?? 0;
+      if (exists?.length) {
+        const target = targetsInSceneRef.current[key].target;
 
-      _scratchBoxRef.current.setFromObject(meshesInSceneRef.current[key].target).getCenter(_scratchCenterRef.current);
-      cameraStopPositionsRef.current[i].copy(_scratchCenterRef.current);
+        if (typeof target['updateWorldMatrix'] === 'function') target.updateWorldMatrix(true, false);
+        _scratchBoxRef.current.setFromObject(target).getCenter(_scratchCenterRef.current);
+        cameraStopPositionsRef.current[i].copy(_scratchCenterRef.current);
+      }
     }
 
     cameraStopPositionsRef.current.length = length || 1;
@@ -169,7 +258,8 @@ const SceneRig = ({
 
     let nextPosition = cameraStopPositionsRef.current[0];
     const focusTargetExists = selection.isFocused !== null && selection.isFocused?.length > 0;
-    const focusedIndex = focusTargetExists ? (meshesInSceneRef.current[selection.isFocused]?.index ?? -1) : -1;
+    const focusedTargetUUID = !focusTargetExists ? -1 : nameToUUIDRef.current[selection.isFocused];
+    const focusedIndex = focusTargetExists ? (targetsInSceneRef.current[focusedTargetUUID]?.index ?? -1) : -1;
     const isManualOverrideActive = elapsedTime < manualOverrideTimeRef.current;
 
     if (focusedIndex >= 0 && cameraStopPositionsRef.current[focusedIndex]) {
@@ -178,11 +268,9 @@ const SceneRig = ({
     }
     else if (isManualOverrideActive && cameraStopPositionsRef.current[targetIndexRef.current]) {
       // if current index points to a valid entry in cameraStopPositionsRef then prevent the else block from running.
-      // if false (e.g. isManualOverrideActive is false or cameraStopPositionsRef.current is shortened between frames by useEffect),
-      // then the else block resets the index. 
+      // if false (e.g. isManualOverrideActive is false or cameraStopPositionsRef.current is shortened between frames by useEffect), then the else block resets the index.
     }
     else {
-      // currentCameraPositionRef.current.copy(cameraStopPositionsRef.current[targetIndexRef.current]);  
       let currentIndex = targetIndexRef.current;
       let nextIndex = currentIndex >= cameraStopPositionsRef.current.length - 1 ? 0 : currentIndex + 1;
       const canSwitch = (elapsedTime - lastSwitchTimeRef.current) > MIN_DWELL_SECONDS;
@@ -194,26 +282,24 @@ const SceneRig = ({
       }
     }
 
-    const targetName = targets[targetIndexRef.current]?.name;
-    const meshInScene = meshesInSceneRef.current[targetName]?.target;
-    const meshInSceneName = meshInScene?.name;
-    const isTargetInScene = (targetName?.length && meshInSceneName?.length) && (targetName === meshInSceneName);
+    const targetUUID = targets[targetIndexRef.current]?.uuid;
+    const targetInScene = targetsInSceneRef.current[targetUUID]?.target;
+    const targetInSceneUUID = targetInScene?.uuid;
+    const isTargetInScene = (targetUUID?.length && targetInSceneUUID?.length) && (targetUUID === targetInSceneUUID);
 
-    if (isTargetInScene && meshInScene.isObject3D) {
-      // if (prevTargetIndexRef.current !== targetIndexRef.current) {
-        if (typeof meshInScene['updateWorldMatrix'] === 'function') meshesInSceneRef.current[targetName].target.updateWorldMatrix(true, false);
-        _scratchBoxRef.current.setFromObject(meshInScene).getCenter(_scratchCenterRef.current);
-      //   prevTargetIndexRef.current = targetIndexRef.current;
-      // }
+    if (isTargetInScene && targetInScene.isObject3D) {
+      // if (prevTargetIndexRef.current !== targetIndexRef.current || cameraStopPositionsRef.current[targetIndexRef.current].distanceTo(_scratchCenterRef.current) > EPSILON_10e4) {
+      getAABBCenterFast(targetInScene, _scratchCenterRef.current);
+      cameraStopPositionsRef.current[targetIndexRef.current].copy(_scratchCenterRef.current);
+      prevTargetIndexRef.current = targetIndexRef.current;
       nextPosition = _scratchCenterRef.current;
+      // }
     }
     else {
       nextPosition = cameraStopPositionsRef.current[targetIndexRef.current];
     }
 
     nextCameraPositionRef.current.set(nextPosition.x + xOffset, nextPosition.y + yOffset, nextPosition.z + zOffset);
-    // cameraTargetPositionRef.current.set(currentCameraPositionRef.current.x, currentCameraPositionRef.current.y, currentCameraPositionRef.current.z - OFFSET_CAMERA_POSITION[2]);
-    // camera.lookAt(cameraTargetPositionRef.current);
     easing.damp3(camera.position, nextCameraPositionRef.current, 1, clampedDelta);
   });
 };
