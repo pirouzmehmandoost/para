425a6a2530d1224fd7ebaaed2a28e3c34e9a9158
pirouzmehmandoost <pirouzmehmandoost@gmail.com>
2026-05-31 22:43:41 -0700

    remove imgUrls property on all elements of portfolio.projects

--- src/lib/configs/globals.js
+++ src/lib/configs/globals.js
@@ -23,21 +23,6 @@ export const portfolio = {
           Large enough to fit over your shoulder and thin enough to carry with comfort. Lightweight and satisfyingly squishy. 
           Large enough to fit over your shoulder and thin enough to carry with comfort. Lightweight and satisfyingly squishy.`,
         dimensions:  `21.5 x 5" x 14"`,
-        imgUrls: {
-          bannerUrl: '/oval_bag_v8.5_matte_white_5.png',
-          matte_black: [
-            '/oval_bag_matte_black_1.png',
-            '/oval_bag_matte_black_2.png',
-            '/oval_bag_matte_black_3.png',
-            '/oval_bag_matte_black_4.png',
-          ],
-          matte_white: [
-            '/oval_bag_matte_white_1.png',
-            '/oval_bag_matte_white_2.png',
-            '/oval_bag_matte_white_3.png',
-            '/oval_bag_matte_white_4.png',
-          ],
-        },
         materialSpecs: 'Thermoplastic urethane',
         shortDescription: `A large 3D printed shoulder bag.`,
         weight: '1lb',
@@ -67,21 +52,6 @@ export const portfolio = {
         care: 'Hand wash with soap. Avoid contact with solvents containing acetone.',
         description: 'Large enough to fit essentials, small enough second as a table decoration. Lightweight and satisfyingly squishy.',
         dimensions:  `16 x 4" x 12"`,
-        imgUrls: {
-          bannerUrl: '/oval_bag_v8.5_matte_white_5.png',
-          matte_black: [
-            '/oval_bag_matte_black_1.png',
-            '/oval_bag_matte_black_2.png',
-            '/oval_bag_matte_black_3.png',
-            '/oval_bag_matte_black_4.png',
-          ],
-          matte_white: [
-            '/oval_bag_matte_white_1.png',
-            '/oval_bag_matte_white_2.png',
-            '/oval_bag_matte_white_3.png',
-            '/oval_bag_matte_white_4.png',
-          ],
-        },
         materialSpecs: 'Thermoplastic urethane',
         shortDescription: 'A small 3D printed shoulder bag.',
         weight: '1lb',
@@ -97,8 +67,8 @@ export const portfolio = {
         },
         groupName: 'Sang',
         materials: {
-          defaultMaterialID: 'matte_black',
-          materialIDs: ['matte_black', 'translucent_grey'],
+          defaultMaterialID: 'translucent_grey',
+          materialIDs: ['translucent_grey', 'matte_black'],
         },
         rotation: { x: 0, y: 0, z: 0 },
         rotationSpeed: 0.45,
@@ -111,21 +81,6 @@ export const portfolio = {
         care: 'Hand wash with soap. Avoid contact with solvents containing acetone.',
         description: `A Yoga mat holder with a large handle for easy carrying. Lightweight to allow sitting rolled mats upright, adding sculptural qualities to idle exercise equipment.`,
         dimensions:  `21.5 x 5" x 14"`,
-        imgUrls: {
-          bannerUrl: '/yoga_mat_strap_metallic_perspective_quarter.png',
-          gloss_black: [
-            '/yoga_mat_strap_metallic_perspective_side.png',
-            '/yoga_mat_strap_metallic_perspective_top.png',
-            '/yoga_mat_strap_metallic_perspective_front.png',
-            '/yoga_mat_strap_metallic_perspective_quarter.png',
-          ],
-          matte_black: [
-            '/yoga_mat_strap_top.png',
-            '/yoga_mat_strap_front.png',
-            '/yoga_mat_strap_side.png',
-            '/yoga_mat_strap_quarter.png',
-          ],
-        },
         materialSpecs: 'Thermoplastic urethane',
         shortDescription: `A yoga mat holder with a large handle.`,
         weight: '1lb',
