c08411341b67f3fc0c50ca2e71b4cc96afdb1116
pirouzmehmandoost <pirouzmehmandoost@gmail.com>
2026-07-29 17:57:01 -0700

    Update comments

--- src/lib/configs/carouselConfigs.js
+++ src/lib/configs/carouselConfigs.js
@@ -1,10 +1,10 @@
 const carouselConfigs = {
-  MIN_DWELL_SECONDS: 12, // dwell time for automatic cycling
-  SWIPE_DELTA_DISTANCE: 0.2, //percentage of viewport dimensions
-  SWIPE_DELTA_PX: 50, // pixel distances, usage will be deprecated soon.
-  MANUAL_OVERRIDE_SECONDS: 15, // dwell time for manual cycling/swipe gestures
-  SWIPE_DELAY_MS: 200, // wait time between calculating swipe gestures
-  SWIPE_DELTA_TIME_MS: 600, // max time between start and end of a swipe gesture
+  MIN_DWELL_SECONDS: 12, // Dwell time for Carousel automatic cycling.
+  SWIPE_DELTA_DISTANCE: 0.2, // Min length of swipe gestures as a fraction of NDC width, delta x between pointerDown and pointerUp MouseEvents.
+  SWIPE_DELTA_PX: 50, // Min distance between CSS pixels on pointerDown and pointerUp events, usage will be deprecated soon.
+  MANUAL_OVERRIDE_SECONDS: 15, // Dwell time for manual cycling, i.e. how long Carousel will dwell after a swipe gesture moves it.
+  SWIPE_DELAY_MS: 200, // Latency period between calculating swipe gestures.
+  SWIPE_DELTA_TIME_MS: 600, // Max time between start and end of a swipe gesture.
 };
 
 export default carouselConfigs;
