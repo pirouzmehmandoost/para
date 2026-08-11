52a58a12577ea0043395c6a557503a24aae5fece
pirouzmehmandoost <pirouzmehmandoost@gmail.com>
2026-08-11 13:42:13 -0700

    Update comments and format

--- src/app/stores/materialStore.js
+++ src/app/stores/materialStore.js
@@ -9,7 +9,6 @@ const _buildCacheKey = (obj) => {
   return Object.keys(obj).sort().join('|');
 };
 
-/** See materialStore.md §4 **/
 const width1K = 32;
 const height1K = 32;
 const size1K = width1K * height1K;
@@ -52,47 +51,15 @@ for (let i = 0; i < size1K; i++) {
 }
 
 
-// function _generateDataTextures() {
-//   const bumpDataTexture = new THREE.DataTexture(bumpData, width1K, height1K);
-//   bumpDataTexture.name = '_scratchBumpTexture';
-//   bumpDataTexture.colorSpace = THREE.NoColorSpace;
-//   bumpDataTexture.needsUpdate = true;
-
-//   const diffuseDataTexture = new THREE.DataTexture(diffuseData, width1K, height1K);
-//   diffuseDataTexture.name = '_scratchDiffuseTexture';
-//   diffuseDataTexture.colorSpace = THREE.SRGBColorSpace;
-//   diffuseDataTexture.needsUpdate = true;
-
-//   const roughnessDataTexture = new THREE.DataTexture(roughnessData, width1K, height1K);
-//   roughnessDataTexture.name = '_scratchRoughnessTexture';
-//   roughnessDataTexture.colorSpace = THREE.NoColorSpace;
-//   roughnessDataTexture.needsUpdate = true;
-
-//   // const transmissionDataTexture = new THREE.DataTexture(transmissionData, width1K, height1K);
-//   // transmissionDataTexture.name = '_scratchTransmissionTexture';
-//   // transmissionDataTexture.colorSpace = THREE.NoColorSpace;
-//   // transmissionDataTexture.needsUpdate = true;
-
-//   return { bumpDataTexture, diffuseDataTexture, roughnessDataTexture };
-//   // return { bumpDataTexture, diffuseDataTexture, roughnessDataTexture, transmissionDataTexture };
-// };
-
-// scratchTextures[`${normalDataTexture.name }`] = normalDataTexture;
-
-/*
- * returns an object with properties of type THREE.DataTexture.
- * These are assigned to the bumpMap, map, roughnessMap, transmissionMap properties of material config objects:
- *  - The properties of private constant _meshPhysicalMaterialConfigs
- *    (e.g. _meshPhysicalMaterialConfigs.glossBlackMaterial, _meshPhysicalMaterialConfigs.stainedMatteBlackMaterial, etc.).
- *  - The exported constant defaultMeshPhysicalMaterialConfig (imported in Model.js).
-*/
+// returns an object with properties of type THREE.DataTexture. 
+// These are assigned to as the bumpMap, map, roughnessMap, transmissionMap properties of material config objects
 function _generateDataTextures({
   bumpMap = false,
   map = false,
   displacementMap = false,
   normalMap = false,
   roughnessMap = false,
-  transmissionMap = false
+  // transmissionMap = false
 }) {
   let scratchTextures = {};
 
@@ -101,7 +68,7 @@ function _generateDataTextures({
     bumpDataTexture.name = '_scratchBumpTexture';
     bumpDataTexture.colorSpace = THREE.NoColorSpace;
     bumpDataTexture.needsUpdate = true;
-    scratchTextures = { ...scratchTextures, bumpDataTexture };
+    scratchTextures.bumpMap = bumpDataTexture;
   };
 
   if (map) {
@@ -109,7 +76,7 @@ function _generateDataTextures({
     diffuseDataTexture.name = '_scratchDiffuseTexture';
     diffuseDataTexture.colorSpace = THREE.SRGBColorSpace;
     diffuseDataTexture.needsUpdate = true;
-    scratchTextures = { ...scratchTextures, diffuseDataTexture };
+    scratchTextures.map =  diffuseDataTexture;
   };
 
   if (displacementMap) {
@@ -117,7 +84,7 @@ function _generateDataTextures({
     displacementDataTexture.name = '_scratchDisplacementTexture';
     displacementDataTexture.colorSpace = THREE.NoColorSpace;
     displacementDataTexture.needsUpdate = true;
-    scratchTextures = { ...scratchTextures, displacementDataTexture };
+    scratchTextures.displacementMap = displacementDataTexture;
   };
 
   if (normalMap) {
@@ -125,7 +92,7 @@ function _generateDataTextures({
     normalDataTexture.name = '_scratchNormalTexture';
     normalDataTexture.colorSpace = THREE.NoColorSpace;
     normalDataTexture.needsUpdate = true;
-    scratchTextures = { ...scratchTextures, normalDataTexture };
+    scratchTextures.normalMap = normalDataTexture;
   };
 
   if (roughnessMap) {
@@ -133,63 +100,47 @@ function _generateDataTextures({
     roughnessDataTexture.name = '_scratchRoughnessTexture';
     roughnessDataTexture.colorSpace = THREE.NoColorSpace;
     roughnessDataTexture.needsUpdate = true;
-    scratchTextures = { ...scratchTextures, roughnessDataTexture };
+    scratchTextures.roughnessMap = roughnessDataTexture;
   };
 
-  if (transmissionMap) {
-    const transmissionDataTexture = new THREE.DataTexture(transmissionData, width1K, height1K);
-    transmissionDataTexture.name = '_scratchTransmissionTexture';
-    transmissionDataTexture.colorSpace = THREE.NoColorSpace;
-    transmissionDataTexture.needsUpdate = true;
-    scratchTextures = { ...scratchTextures, transmissionDataTexture };
-  };
+  // if (transmissionMap) {
+  //   const transmissionDataTexture = new THREE.DataTexture(transmissionData, width1K, height1K);
+  //   transmissionDataTexture.name = '_scratchTransmissionTexture';
+  //   transmissionDataTexture.colorSpace = THREE.NoColorSpace;
+  //   transmissionDataTexture.needsUpdate = true;
+  //   scratchTextures.transmissionMap = transmissionDataTexture;
+  // };
 
   return scratchTextures;
 };
 
 const _defaultMeshPhysicalMaterialConfigMaps = _generateDataTextures({ bumpMap: true, map: true, roughnessMap: true, });
 
-// const _groundGridMeshPhysicalMaterialConfigMaps = _generateDataTextures({normalMap: true, displacementMap: true });
-
 export const defaultMeshPhysicalMaterialConfig = {
-  bumpMap: _defaultMeshPhysicalMaterialConfigMaps.bumpDataTexture,
+  bumpMap: _defaultMeshPhysicalMaterialConfigMaps.bumpMap,
   color: '#2f2f2f',
   clearcoat: EPSILON_1e7,
   clearcoatRoughness: 1,
   flatShading: false,
-  map: _defaultMeshPhysicalMaterialConfigMaps.diffuseDataTexture,
-  roughnessMap: _defaultMeshPhysicalMaterialConfigMaps.roughnessDataTexture,
+  map: _defaultMeshPhysicalMaterialConfigMaps.map,
+  roughnessMap: _defaultMeshPhysicalMaterialConfigMaps.roughnessMap,
   side: THREE.DoubleSide,
   // thickness: 0,
   // transmission: EPSILON_1e7,
-  // transmissionMap: _defaultMeshPhysicalMaterialConfigMaps.transmissionDataTexture,
-  // transparent: true, // true for all MeshPysicalMaterials in store
+  // transmissionMap: _defaultMeshPhysicalMaterialConfigMaps.transmissionMap,
+  // transparent: true,
 };
-
-// export const groundGridMeshPhysicalMaterialConfig = {
-//   color: '#2f2f2f',
-//   clearcoat: EPSILON_1e7,
-//   clearcoatRoughness: 1,
-//   flatShading: false,
-//   side: THREE.DoubleSide,
-//   map: _defaultMeshPhysicalMaterialConfigMaps.diffuseDataTexture,
-//   normalMap: _groundGridMeshPhysicalMaterialConfigMaps.normalDataTexture,
-//   displacementMap: _groundGridMeshPhysicalMaterialConfigMaps.displacementDataTexture,
-// };
 
 export const defaultMeshStandardMaterialConfig = {
   color: '#101010',
   flatShading: false,
   metalness: 0.8,
-  name: 'ground',
-  opacity: 1,
   roughness: 1,
   side: THREE.DoubleSide,
-  transparent: true,
 };
 
 const _meshPhysicalMaterialConfigs = {
-  glossBlackMaterial: {
+  gloss_black: {
     bumpScale: 2,
     color: '#101010',
     flatShading: false,
@@ -199,7 +150,7 @@ const _meshPhysicalMaterialConfigs = {
     roughness: 0.375,
     side: THREE.DoubleSide,
   },
-  matteBlackMaterial: {
+  matte_black: {
     bumpScale: 3,
     color: '#2c2c2c',
     flatShading: false,
@@ -209,7 +160,7 @@ const _meshPhysicalMaterialConfigs = {
     roughness: 0.75,
     side: THREE.DoubleSide,
   },
-  stainedMatteBlackMaterial: {
+  stained_matte_black: {
     bumpScale: -10,
     color: '#6f6f6f',
     flatShading: false,
@@ -219,39 +170,10 @@ const _meshPhysicalMaterialConfigs = {
     roughness: 0.8,
     side: THREE.DoubleSide,
   },
-  // translucentGreyMaterial: {
-  //   bumpScale: 1,
-  //   color: '#949994',
-  //   clearcoat: 0.8,
-  //   clearcoatRoughness: 0.3,
-  //   flatShading: false,
-  //   ior: 1.5,
-  //   name: 'translucent_grey',
-  //   reflectivity: 0.25,
-  //   roughness: 0.3,
-  //   thickness: 20,
-  //   transmission: 1,
-  //   transparent: true,
-  //   side: THREE.DoubleSide,
-  // },
-  // groundGridMaterial: {
-  //   ...groundGridMeshPhysicalMaterialConfig,
-  //   bumpScale: 1,
-  //   color: '#949994',
-  //   displacementBias: -0.05,
-  //   displacementScale: 18,
-  //   flatShading: false,
-  //   ior: 1.8,
-  //   name: 'ground_grid',
-  //   normalScale: new THREE.Vector2(0, 0.5),
-  //   reflectivity: 0.35,
-  //   roughness: 1,
-  //   side: THREE.DoubleSide,
-  // },
 }
 
 for (const materialConfig in _meshPhysicalMaterialConfigs) {
-  // set clearcoat, clearcoatRoughness, and transmission if undefined or 0. 
+  // set clearcoat and transmission to epsilon value if undefined or 0. 
   if (!_meshPhysicalMaterialConfigs[materialConfig]?.clearcoat) {
     _meshPhysicalMaterialConfigs[materialConfig].clearcoat = defaultMeshPhysicalMaterialConfig.clearcoat;
   }
@@ -261,7 +183,6 @@ for (const materialConfig in _meshPhysicalMaterialConfigs) {
   // }
 
   // set clearcoatRoughness to 1 if undefined or 0. 
-  // Transitioning from 0 will produces an undesirable, highly concentrated specular highlights.
   // Materials with intentionally imperceptible clearcoat have clearcoat = 1e-7 and clearcoatRoughness = 1.
   if (!_meshPhysicalMaterialConfigs[materialConfig]?.clearcoatRoughness) {
     _meshPhysicalMaterialConfigs[materialConfig].clearcoatRoughness = defaultMeshPhysicalMaterialConfig.clearcoatRoughness;
@@ -279,15 +200,11 @@ for (const materialConfig in _meshPhysicalMaterialConfigs) {
   // _meshPhysicalMaterialConfigs[materialConfig].transmissionMap = dataTextures.transmissionDataTexture;
 }
 
-// const groundGridDataTextures = _generateDataTextures({ normalMap: true, displacementMap: true });
-// _meshPhysicalMaterialConfigs.groundGridMaterial.normalMap = groundGridDataTextures.normalDataTexture;
-// _meshPhysicalMaterialConfigs.groundGridMaterial.displacementMap = groundGridDataTextures.displacementDataTexture;
-
 const materialState = {
   gloss_black: {
     displayName: 'Gloss Black',
     tailwindColor: `bg-radial-[at_40%_35%] from-zinc-500 via-zinc-950 via-37% to-zinc-500 to-100%`,
-    material: new THREE.MeshPhysicalMaterial({ ..._meshPhysicalMaterialConfigs.glossBlackMaterial }),
+    material: new THREE.MeshPhysicalMaterial({ ..._meshPhysicalMaterialConfigs.gloss_black }),
     textures: {
       bumpMap: '/gloss_material_roughness.jpg',
     },
@@ -295,22 +212,12 @@ const materialState = {
   ground: {
     displayName: 'Ground',
     tailwindColor: `bg-zinc-900`,
-    material: new THREE.MeshStandardMaterial({ ...defaultMeshStandardMaterialConfig }),
+    material: new THREE.MeshStandardMaterial({ ...defaultMeshStandardMaterialConfig, name: 'ground', }),
   },
-  // ground_grid: {
-  //   displayName: 'ground_grid',
-  //   tailwindColor: `bg-zinc-900`,
-  //   material: new THREE.MeshPhysicalMaterial({ ..._meshPhysicalMaterialConfigs.groundGridMaterial}),
-  //   textures: {
-  //     // map: '/ground_diffuse_map.jpg',
-  //     normalMap: '/ground_normal_map.jpg',
-  //     displacementMap: '/ground_displacement_map_3.png',
-  //   },
-  // },
   matte_black: {
     displayName: 'Matte Black',
     tailwindColor: `bg-radial-[at_35%_35%] from-zinc-500 to-zinc-900 to-65%`,
-    material: new THREE.MeshPhysicalMaterial({ ..._meshPhysicalMaterialConfigs.matteBlackMaterial }),
+    material: new THREE.MeshPhysicalMaterial({ ..._meshPhysicalMaterialConfigs.matte_black }),
     textures: {
       bumpMap: '/gloss_material_roughness.jpg',
     },
@@ -318,18 +225,13 @@ const materialState = {
   stained_matte_black: {
     displayName: 'Stained Matte Black',
     tailwindColor: `bg-radial-[at_35%_35%] from-zinc-500 to-zinc-900 to-65%`,
-    material: new THREE.MeshPhysicalMaterial({ ..._meshPhysicalMaterialConfigs.stainedMatteBlackMaterial }),
+    material: new THREE.MeshPhysicalMaterial({ ..._meshPhysicalMaterialConfigs.stained_matte_black }),
     textures: {
       map: '/stained_matte_black_diffuse_closed_bag.jpg',
       roughnessMap: '/stained_matte_black_roughness_closed_bag.jpg',
       bumpMap: '/stained_matte_black_bump_closed_bag.jpg',
     },
   },
-  // translucent_grey: {
-  //   displayName: 'Translucent Grey',
-  //   tailwindColor: `bg-radial-[at_45%_45%] from-orange-50 from-3% via-stone-600 via-55% to-slate-950 to-95%`,
-  //   material: new THREE.MeshPhysicalMaterial({ ..._meshPhysicalMaterialConfigs.translucentGreyMaterial }),
-  // },
 };
 
 const _selectedMaterialsCache = new Map();
