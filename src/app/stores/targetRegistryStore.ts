b011aa4dad997ea0166768d88e3c1fd496e61544
pirouzmehmandoost <pirouzmehmandoost@gmail.com>
2026-08-11 15:01:22 -0700

    format file

--- src/app/stores/targetRegistryStore.ts
+++ src/app/stores/targetRegistryStore.ts
@@ -1,52 +1,61 @@
-import { create } from 'zustand';
-import * as THREE from 'three';
-import TargetRegistry from '@targetRegistry';
+import { create } from 'zustand'
+import * as THREE from 'three'
+import TargetRegistry from '@targetRegistry'
 
-type Targets = THREE.Object3D[] | ((obj: THREE.Object3D) => boolean);
+type Targets = THREE.Object3D[] | ((obj: THREE.Object3D) => boolean)
 
-type TargetRegistryStore = {
-  registry: TargetRegistry | null;
-  _uuid: string | null;
-  initialize: (scene: THREE.Scene, targets: Targets) => boolean;
-  reset: () => void;
-};
+type TargetRegistryState = {
+  registry: TargetRegistry | null
+  _uuid: string | null
+  initialize: (scene: THREE.Scene, targets: Targets) => boolean
+  reset: () => void
+}
 
-const initialState: { registry: TargetRegistry | null; _uuid: string | null } = {
+const initialState: {
+  registry: TargetRegistry | null
+  _uuid: string | null
+} = {
   registry: null,
   _uuid: null,
-};
+}
 
-const useTargetRegistry = create<TargetRegistryStore>()((set, get) => ({
+const useTargetRegistry = create<TargetRegistryState>()((set, get) => ({
   ...initialState,
 
   initialize: (scene: THREE.Scene, targets: Targets) => {
-    if (!scene?.isScene) return false;
+    if (!scene?.isScene) return false
 
-    const { registry, _uuid } = get();
-    if (!!registry && scene.uuid === _uuid) return false;
+    const { registry, _uuid } = get()
+    if (!!registry && scene.uuid === _uuid) return false
 
-    registry?.deregister();
-    const newRegistry = new TargetRegistry(scene);
-    const sceneUUID = scene.uuid;
+    registry?.deregister()
 
-    if (Array.isArray(targets)) newRegistry.register(targets);
-    else newRegistry.registerByFilter(targets);
+    const newRegistry = new TargetRegistry(scene)
+    const sceneUUID = scene.uuid
+
+    if (Array.isArray(targets)) {
+      newRegistry.register(targets)
+    }
+    else {
+      newRegistry.registerByFilter(targets)
+    }
 
     set({
       registry: newRegistry,
       _uuid: sceneUUID,
-    });
-    return true;
+    })
+
+    return true
   },
 
   reset: () => {
-    const reg = get().registry;
+    const reg = get().registry
 
     if (!!reg) {
-      reg.deregister();
-      set({ ...initialState });
+      reg.deregister()
+      set({ ...initialState })
     }
   },
-}));
+}))
 
-export default useTargetRegistry;
+export default useTargetRegistry
