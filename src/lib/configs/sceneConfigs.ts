ef365ee6751d6efc026f4673ca5bee4a6d6a7e8c
pirouzmehmandoost <pirouzmehmandoost@gmail.com>
2026-08-19 02:31:27 -0700

    update configs

--- src/lib/configs/sceneConfigs.ts
+++ src/lib/configs/sceneConfigs.ts
@@ -1,9 +1,4 @@
-interface SceneConfigs {
-  ENV_IMG_URL: string
-  BACKGROUND_COLOR: string
-}
-
-const sceneConfigs: SceneConfigs = {
+const sceneConfigs = {
   ENV_IMG_URL: '/para_ground_glare_fog.hdr',
   BACKGROUND_COLOR: '#bcbcbc',
 }
