062cc02d5d85605ed80a3caaaf94021e327baae5
pirouzmehmandoost <pirouzmehmandoost@gmail.com>
2026-09-02 18:13:36 -0700

    Update MainMenu and menuStore variable names

--- src/app/components/ui/MainMenu.tsx
+++ src/app/components/ui/MainMenu.tsx
@@ -1,96 +1,80 @@
 'use client'
-import { memo, useEffect, useCallback, useMemo } from 'react'
+import { memo, useEffect, useCallback } from 'react'
+import type { MouseEvent } from 'react'
 import Link from 'next/link'
 import MenuIcon from '@mui/icons-material/Menu'
 import CloseIcon from '@mui/icons-material/Close'
 import useMenu from '@stores/menuStore'
 
-// interface MenuLink {
-//   title: string
-//   href: string
-//   disabled?: boolean
-// }
-
-// const links: MenuLink[] = [
-//   { disabled: false, title: 'LinkedIn', href: 'https://www.linkedin.com/in/pirouzmehmandoost/' },
-//   { disabled: false, title: 'Github', href: 'https://github.com/pirouzmehmandoost/para/blob/main/README.md' },
-// ]
-
 const setVisible = useMenu.getState().setVisible
 
-interface MenuButtonProps { phase: boolean }
-const ToggleMainMenuButton = memo(({ phase }: MenuButtonProps) => {
+interface MenuButtonProps {
+  callback: (e: MouseEvent<HTMLButtonElement>, state: boolean) => void
+  state: boolean
+}
+const MenuButton = memo(({ callback, state }: MenuButtonProps) => {
   return (
     <button
       id='main-menu-button'
       aria-label='toggle-main-menu'
-      className='fixed w-fit h-fit z-30 inset-0 top-6 left-6 p-2 rounded-full cursor-pointer backdrop-contrast-125 backdrop-blur-xl transition-all transition-discrete duration-500 ease-in-out text-neutral-600 hover:text-neutral-700 bg-neutral-600/20 hover:bg-neutral-600/30'
-      onClick={() => setVisible(!phase)}
+      className={`appearance-none flex grow w-fit h-fit p-2 sm:p-2 md:p-3 lg:p-4 xl:p-4 2xl:p-4 rounded-full items-center text-header cursor-pointer transition-all transition-discrete duration-300 delay-200 ease-out hover:text-header/50 ${state === false ? 'backdrop-blur-md bg-container/20' : 'backdrop-blur-none bg-container/0'}`}
+      onClick={(e) => callback(e, !state)}
       type='button'
     >
-      {phase === false ? <MenuIcon fontSize='large' /> : <CloseIcon fontSize='large' />}
+      {state === false ? <MenuIcon fontSize='medium' /> : <CloseIcon fontSize='medium' />}
     </button>
   )
 })
-ToggleMainMenuButton.displayName = "ToggleMainMenuButton"
+MenuButton.displayName = 'MenuButton'
 
 const MainMenu = () => {
   const menuVisible = useMenu(state => state.menuState.visible)
-  const firstPageVisited = useMenu((s) => s.firstPageVisited)
+  const isFirstPageVisit = useMenu((s) => s.isFirstPageVisit)
   const hasHydrated = useMenu((s) => s.hasHydrated)
 
   useEffect(() => {
-    if (hasHydrated && !firstPageVisited) {
+    if (hasHydrated && !isFirstPageVisit) {
       useMenu.getState().setPageVisited()
       setVisible(true)
     }
-  }, [hasHydrated, firstPageVisited])
+  }, [hasHydrated, isFirstPageVisit])
 
-  const handleClick = useCallback((e: React.MouseEvent<HTMLAnchorElement>, disabled: boolean) => {
-    setVisible(false)
-    if (disabled) e.preventDefault()
+  const handleClick = useCallback((e: MouseEvent<HTMLButtonElement>, state: boolean) => {
+    setVisible(state)
+    if (state === false) e.preventDefault()
   }, [])
 
-  //  <div className='text-9xl text-blue-800 opacity-100 text-center self-center place-self-center-safe justify-center-safe select-none touch-none transition-all transition-discrete duration-1000 delay-2000 ease-in will-change-auto starting:opacity-0'>
-  //     Test
-  //   </div>
-
   return (
-    <div className='relative flex flex-col w-full h-full'>
-      <ToggleMainMenuButton phase={menuVisible} />
-      <div className={`fixed flex flex-col z-20 justify-center items-center inset-0 text-neutral-800 uppercase transition-all transition-discrete duration-500 ease-in-out ${menuVisible ? 'opacity-100 pointer-events-auto' : 'opacity-0 pointer-events-none'}`}>
-        {/* {menuVisible && ( */}
-        <div className='flex flex-col w-full h-full space-y-6 p-6'>
-          <div className='flex flex-col w-fit h-fit mt-13 p-6 space-y-6 rounded-4xl bg-neutral-500/20 backdrop-blur-xl'>
-
-          <div className='w-fit h-fit place-self-center-safe text-nowrap sm:text-3xl md:text-3xl lg:text-4xl xl:text-4xl 2xl:text-4xl'>
-            Pirouz Mehmandoost
-          </div>
-            <div className='flex flex-col w-full h-full space-y-6'>
-              <Link
-                className='sm:text-lg md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl cursor-pointer transition-all transition-discrete duration-500 ease-in-out hover:text-neutral-500'
-                id='main-menu-link-linkedin'
-                href={'https://www.linkedin.com/in/pirouzmehmandoost/'}
-                rel='noopener noreferrer'
-                target='_blank'
-              >
-                LinkedIn
-              </Link>
-              <Link
-                className='sm:text-lg md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl cursor-pointer transition-all transition-discrete duration-500 ease-in-out hover:text-neutral-500'
-                id='main-menu-link-github'
-                href={'https://github.com/pirouzmehmandoost/para/blob/main/README.md'}
-                rel='noopener noreferrer'
-                target='_blank'
-              >
-                Github
-              </Link>
-            </div>
-          </div>
-        </div>
-        {/* )} */}
+    <div className='flex flex-col grow place-items-start max-w-fit max-h-fit p-6 text-body select-none'>
+      <div className='fixed z-30'>
+        <MenuButton callback={handleClick} state={menuVisible} />
       </div>
-      {/* <div className={`fixed inset-0 z-10 bg-neutral-400/0 backdrop-blur-md md:backdrop-blur-xl select-none pointer-events-none transition-all transition-discrete duration-500 ease-in-out ${menuVisible ? 'opacity-100' : 'opacity-0'}`} /> */}
+      <div className={`flex flex-col grow max-w-fit max-h-fit z-20 p-4 space-y-6 rounded-3xl bg-container/20 backdrop-blur-md justify-center-safe items-center-safe transition-all transition-discrete duration-500 ease-in-out ${menuVisible ? 'visible opacity-100 pointer-events-auto' : 'collapse opacity-0 pointer-events-none'}`}>
+
+        <div className='flex grow w-fit h-fit pt-6 sm:pt-6 md:pt-8 lg:pt-8 xl:pt-10 2xl:pt-10 place-self-center-safe text-center uppercase text-nowrap text-xl sm:text-xl md:text-2xl lg:text-3xl xl:text-3xl 2xl:text-3xl transition-all transition-discrete duration-500 ease-in-out'>
+          Pirouz Mehmandoost
+        </div>
+        <div className={`flex flex-col w-full h-full space-y-2`}>
+          <Link
+            className='w-fit text:md sm:text-md md:text-md lg:text-lg xl:text-xl 2xl:text-xl cursor-pointer transition-all transition-discrete duration-500 ease-in-out hover:text-neutral-500'
+            id='main-menu-link-linkedin'
+            href={'https://www.linkedin.com/in/pirouzmehmandoost/'}
+            rel='noopener noreferrer'
+            target='_blank'
+          >
+            LinkedIn
+          </Link>
+          <Link
+            className='w-fit text-md sm:text-md md:text-md lg:text-lg xl:text-xl 2xl:text-xl cursor-pointer transition-all transition-discrete duration-500 ease-in-out hover:text-neutral-500'
+            id='main-menu-link-github'
+            href={'https://github.com/pirouzmehmandoost/para/blob/main/README.md'}
+            rel='noopener noreferrer'
+            target='_blank'
+          >
+            Github
+          </Link>
+        </div>
+      </div>
     </div>
   )
 }
